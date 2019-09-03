2c33597044c990bf8945e1b97275e502488ac7ee
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-03 16:51:47 +0200

    Importer diverse NAV-komponenter, fjern cra-stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './app.less';
+import Lenke from 'nav-frontend-lenker';
+import { Element } from 'nav-frontend-typografi';
+import PanelBase from 'nav-frontend-paneler';
+
 const App: React.FC = () => {
-    return <h1 className="tittel">Dette er sykefraværsstatistikk</h1>;
+    return (
+        <div className="app">
+            <div className="stuff">
+                <Lenke href={'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/'}>
+                    Min side arbeidsgiver
+                </Lenke>{' '}
+                > <Element> Sykefraværsstatistikk </Element>
+            </div>
+            <h1 className="tittel">Dette er sykefraværsstatistikk</h1>
+            <PanelBase border>
+                blabla
+            </PanelBase>
+        </div>
+    );
 };
 
 export default App;
