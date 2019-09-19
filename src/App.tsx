d4114cb54e9dd506d1bd1f76d4c1516394a2fe93
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-09-19 15:44:45 +0200

    Legg til Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
 import './app.less';
-import Lenke from 'nav-frontend-lenker';
-import { Element } from 'nav-frontend-typografi';
-import { SykefraværprosentProvider } from './SykefraværprosentProvider';
-import { Statistikk } from './Statistikk';
+import {SykefraværprosentProvider} from './SykefraværprosentProvider';
+import Banner from "./Banner/Banner";
 
 const App: React.FC = () => {
     return (
         <div className="app">
             <SykefraværprosentProvider>
-                <div className="stuff">
-                    <Lenke href={'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/'}>
-                        Min side arbeidsgiver
-                    </Lenke>{' '}
-                    > <Element> Sykefraværsstatistikk </Element>
-                </div>
-                <h1 className="tittel">Dette er sykefraværsstatistikk</h1>
-                <Statistikk />
+                <Banner tekst="Sykefraværsstatistikk" />
             </SykefraværprosentProvider>
         </div>
     );
