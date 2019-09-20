916339339f5da820d8f57677520c14c0db239b79
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-09-20 13:35:43 +0200

    Legg til Infopanel

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './app.less';
 import {SykefraværprosentProvider} from './SykefraværprosentProvider';
 import Banner from "./Banner/Banner";
+import Forside from "./Forside/Forside";
 
 const App: React.FC = () => {
     return (
         <div className="app">
             <SykefraværprosentProvider>
                 <Banner tekst="Sykefraværsstatistikk" />
+                <Forside/>
             </SykefraværprosentProvider>
         </div>
     );
---
+++ src/Forside/Forside.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Infopanel from "../Infopanel/Infopanel";
+import "./Forside.less";
+
+const Forside : React.FunctionComponent = () => {
+
+    return (
+        <div className="forside">
+            <Infopanel/>
+        </div>
+    );
+
+}
+
+export default Forside;
