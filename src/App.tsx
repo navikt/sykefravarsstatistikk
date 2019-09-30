a2ba9d03a8338b2ef36a7b19455cb3e1cc0ed187
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-30 11:58:56 +0200

    første forsøk..

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import './app.less';
-import {SammenligningProvider} from './SammenligningProvider';
+import { SammenligningProvider } from './SammenligningProvider';
 import Banner from './Banner/Banner';
 import Forside from './Forside/Forside';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
     return (
         <div className="app">
             <SammenligningProvider>
-                <Banner tekst="Sykefraværsstatistikk" />
-                <Forside/>
+                <BrowserRouter basename={'/sykefravarsstatistikk'}>
+                    <Banner tekst="Sykefraværsstatistikk" />
+                    <Route path="/" exact={true} component={Forside}/>
+                </BrowserRouter>
             </SammenligningProvider>
         </div>
     );
--- src/Banner/Banner.tsx
+++ src/Banner/Banner.tsx
@@ -1,19 +1,101 @@
 import React from 'react';
 import './Banner.less';
-import {Sidetittel} from 'nav-frontend-typografi';
+import { Sidetittel } from 'nav-frontend-typografi';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import Bedriftsmeny from '@navikt/bedriftsmeny';
+import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 
 interface Props {
     tekst: string;
 }
 
-const Banner: React.FunctionComponent<Props> = props => {
+export interface Organisasjon {
+    Name: string;
+    Type: string;
+    OrganizationNumber: string;
+    OrganizationForm: string;
+    Status: string;
+    ParentOrganizationNumber: string;
+}
+
+export interface JuridiskEnhetMedUnderEnheterArray {
+    JuridiskEnhet: Organisasjon;
+    Underenheter: Array<Organisasjon>;
+}
+
+export const tomAltinnOrganisasjon: Organisasjon = {
+    Name: '',
+    Type: '',
+    OrganizationNumber: '',
+    OrganizationForm: '',
+    Status: '',
+    ParentOrganizationNumber: '',
+};
+
+const liste: JuridiskEnhetMedUnderEnheterArray[] = [
+    {
+        JuridiskEnhet: {
+            Name: 'BALLSTAD OG HAMARØY',
+            Type: 'Business',
+            OrganizationNumber: '811076732',
+            ParentOrganizationNumber: '811076112',
+            OrganizationForm: 'BEDR',
+            Status: 'Active',
+        },
+        Underenheter: [
+            {
+                Name: 'BALLSTAD OG HORTEN',
+                Type: 'Enterprise',
+                ParentOrganizationNumber: '811076732',
+                OrganizationNumber: '811076112',
+                OrganizationForm: 'AS',
+                Status: 'Active',
+            },
+            {
+                Name: 'DIGITAL JUNKIES AS ',
+                Type: 'Enterprise',
+                OrganizationNumber: '822565212',
+                ParentOrganizationNumber: '811076732',
+                OrganizationForm: 'AS',
+                Status: 'Active',
+            },
+            {
+                Name: 'DIGITAL JUNKIES AS ',
+                Type: 'Business',
+                OrganizationNumber: '922658986',
+                ParentOrganizationNumber: '822565212',
+                OrganizationForm: 'BEDR',
+                Status: 'Active',
+            },
+            {
+                Name: 'NAV ENGERDAL',
+                Type: 'Business',
+                ParentOrganizationNumber: '874652202',
+                OrganizationNumber: '991378642',
+                OrganizationForm: 'BEDR',
+                Status: 'Active',
+            },
+            {
+                Name: 'NAV HAMAR',
+                Type: 'Business',
+                ParentOrganizationNumber: '874652202',
+                OrganizationNumber: '990229023',
+                OrganizationForm: 'BEDR',
+                Status: 'Active',
+            },
+        ],
+    },
+];
+
+const Banner: React.FunctionComponent<Props & RouteComponentProps> = props => {
     return (
-        <div className="banner">
-            <div className="banner__innhold">
-                <Sidetittel className="banner__tekst">{props.tekst}</Sidetittel>
-            </div>
-        </div>
+        <Bedriftsmeny
+            sidetittel="Sykefraværsstatistikk"
+            history={props.history}
+            onOrganisasjonChange={() => {}}
+            organisasjonstre={liste}
+        />
     );
 };
 
-export default Banner;
+export default withRouter(Banner);
