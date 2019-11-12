34a10f1623b800fc8dc397322ef3abf0811847b6
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-11-12 09:59:59 +0100

    Fix URL til mock data

--- src/mocking/mock.ts
+++ src/mocking/mock.ts
@@ -2,5 +2,5 @@ import fetchMock from 'fetch-mock';
 import sykefravarsprosent from './sammenligning.json';
 import organisasjoner from './organisasjoner.json';
 
-fetchMock.get('/sykefravarsstatistikk/api/sammenligning', sykefravarsprosent);
+fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
 fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner).spy();
