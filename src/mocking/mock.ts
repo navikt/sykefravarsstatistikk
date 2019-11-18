3bc15f269d403f46418af1968a23c577c135f44d
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2019-11-18 16:40:35 +0100

    Mock ut MSA for å kunne kjøre mot backend lokalt (#15)
    * Mock ut MSA for å kunne kjøre mot backend lokalt
    * Legg til steg i readme
    * Oppdater avhengigheter
    * Bruk v5 av react-collapse

--- src/mocking/mock.ts
+++ src/mocking/mock.ts
@@ -2,5 +2,12 @@ import fetchMock from 'fetch-mock';
 import sykefravarsprosent from './sammenligning.json';
 import organisasjoner from './organisasjoner.json';
 
-fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
-fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner).spy();
+const mockSammenligning = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
+const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner);
+
+if (process.env.REACT_APP_MOCK_ALL) {
+    mockSammenligning();
+    mockMinSideArbeidsgiver().spy()
+} else if (process.env.REACT_APP_MOCK_MSA) {
+    mockMinSideArbeidsgiver().spy()
+}
