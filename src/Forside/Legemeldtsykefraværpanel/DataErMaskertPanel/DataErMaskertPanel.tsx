b044a0ff23cc5e90ae51e790f284cddeb549b11e
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-11-19 16:06:31 +0100

    Bytt ut svg med fil

--- src/Forside/Legemeldtsykefraværpanel/DataErMaskertPanel/DataErMaskertPanel.tsx
+++ src/Forside/Legemeldtsykefraværpanel/DataErMaskertPanel/DataErMaskertPanel.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, default as React } from 'react';
-import InformasjonSirkelIkon from './InformasjonSirkelIkon';
 import './DataErMaskertPanel.less';
+import informasjonsirkelSvg from './informasjon-sirkel.svg';
 
 interface Props {
     label: string;
@@ -8,7 +8,7 @@ interface Props {
 
 export const DataErMaskertPanel: FunctionComponent<Props> = props => (
     <div className="data-er-maskert-panel">
-        <InformasjonSirkelIkon />
+        <img src={informasjonsirkelSvg} className="data-er-maskert-panel__ikon" alt="" />
         {props.label}
     </div>
 );
