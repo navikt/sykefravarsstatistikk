975edf65b0d440b14fba4604bb269c02aa53cc84
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-12-09 16:05:40 +0100

    Tilpasse siden ift skisser

--- src/FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide.tsx
+++ src/FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide.tsx
@@ -1,37 +1,50 @@
 import React from "react";
 import PanelBase from "nav-frontend-paneler";
 import informasjonsirkelSvg from "./informasjon-sirkel.svg";
-import {Normaltekst, Systemtittel, Undertekst, UndertekstBold} from "nav-frontend-typografi";
+import {Element, Normaltekst, Systemtittel} from "nav-frontend-typografi";
+import './ManglerRettigheterIAltinnSide.less'
+import Lenke from "nav-frontend-lenker";
 
 const ManglerRettigheterIAltinnSide: React.FunctionComponent = () => {
     return (
-        <PanelBase className="error-side-panel">
-                <img src={informasjonsirkelSvg} className="error-side-panel__ikon" alt=""/>
-            <div className="infopanel__tekst-wrapper">
-                <Systemtittel className="infopanel__overskrift" tag="h2">
-                    Du mangler rettigheter i Altinn
-                </Systemtittel>
-                <Normaltekst>
-                    Du har ikke Altinn-tilgangen for å se sykefraværsstatistikk for denne virksomheten.
-                </Normaltekst>
-                <UndertekstBold>
-                    For å se sykefraværsstatistikk kan du
-                </UndertekstBold>
-                <Undertekst>
-                    Bytte til en virksomhet der du har tilgang til sykefraværsstatistikk
-                </Undertekst>
-                <Undertekst>
-                    Be om tilgang til Altinn for denne virksomhet
-                </Undertekst>
-                <a
-                    href="https://google.com"
-                    className="lenke"
-                >
-                    Les mer om hvordan tilgangsstyringen i Altinn fungerer
-                </a>
+        <div className="feilside">
+            <PanelBase className="mangler-rettigheter-i-altinn-panel">
+                <div className="mangler-rettigheter-i-altinn-panel__tekst-wrapper">
+                    <div className="mangler-rettigheter-i-altinn-panel__tekst_og_ikon">
+                        <img src={informasjonsirkelSvg} className="mangler-rettigheter-i-altinn-panel__tekst_og_ikon__ikon" alt=""/>
+                        <Systemtittel tag="h2">
+                            Du mangler rettigheter i Altinn
+                        </Systemtittel>
+                    </div>
 
-            </div>
-        </PanelBase>
+                    <div className="mangler-rettigheter-i-altinn-panel__overskrift">
+                        <Normaltekst>
+                            Du har ikke Altinn-tilgangen for å se sykefraværsstatistikk for denne virksomheten.
+                        </Normaltekst>
+                    </div>
+                    <div className="mangler-rettigheter-i-altinn-panel__overskrift">
+                        <Element>
+                            For å se sykefraværsstatistikk kan du
+                        </Element>
+                    </div>
+                    <ul>
+                        <li>
+                            <Normaltekst>
+                                Bytte til en virksomhet der du har tilgang til sykefraværsstatistikk
+                            </Normaltekst>
+                        </li>
+                        <li>
+                            <Normaltekst>
+                                Be om tilgang til Altinn for denne virksomhet
+                            </Normaltekst>
+                        </li>
+                    </ul>
+                    <Lenke href={'https://www.altinn.no/hjelp/profil/roller-og-rettigheter/'}>
+                        Les mer om hvordan tilgangsstyringen i Altinn fungerer
+                    </Lenke>
+                </div>
+            </PanelBase>
+        </div>
     );
 };
 
