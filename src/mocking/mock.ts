40967e38d6cc1eab9a13765a889094495093ced2
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-12-10 15:14:06 +0100

    Slette kommentert kode

--- src/mocking/mock.ts
+++ src/mocking/mock.ts
@@ -3,12 +3,10 @@ import sykefravarsprosent from './sammenligning.json';
 import organisasjoner from './organisasjoner.json';
 
 const mockSammenligning = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
-const mockSammenligningForbidden = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', {status: 403, body: "You are not authorized to view this resource"});
 const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner);
 
 if (process.env.REACT_APP_MOCK_ALL) {
     mockSammenligning();
-    //mockSammenligningForbidden();
     mockMinSideArbeidsgiver().spy()
 } else if (process.env.REACT_APP_MOCK_MSA) {
     mockMinSideArbeidsgiver().spy()
