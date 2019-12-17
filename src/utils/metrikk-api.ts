7545afc4c74dc6c4cddb76fd3e108c10ed629f13
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-17 17:34:27 +0100

    fjern console log

--- src/utils/metrikk-api.ts
+++ src/utils/metrikk-api.ts
@@ -3,6 +3,5 @@ import { FRONTEND_API_PATH } from "../server/konstanter";
 const METRIKK_PATH = FRONTEND_API_PATH + '/metrikker';
 
 export const sendEvent = (event: string): void => {
-    console.log(METRIKK_PATH + '/' + event);
     fetch(METRIKK_PATH + '/' + event, { method: 'POST' });
 };
