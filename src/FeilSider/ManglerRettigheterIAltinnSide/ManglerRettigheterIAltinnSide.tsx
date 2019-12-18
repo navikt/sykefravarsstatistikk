098e45ddb0a3792e8e6e8c63a4e9f2ed146a28f6
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-12-18 16:17:07 +0100

    Forenkle styling for altinn-side

--- src/FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide.tsx
+++ src/FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide.tsx
@@ -1,48 +1,48 @@
-import React from "react";
-import PanelBase from "nav-frontend-paneler";
-import informasjonsirkelSvg from "./informasjon-sirkel.svg";
-import {Element, Normaltekst, Systemtittel} from "nav-frontend-typografi";
-import './ManglerRettigheterIAltinnSide.less'
-import Lenke from "nav-frontend-lenker";
+import React from 'react';
+import PanelBase from 'nav-frontend-paneler';
+import informasjonsirkelSvg from './informasjon-sirkel.svg';
+import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import './ManglerRettigheterIAltinnSide.less';
+import Lenke from 'nav-frontend-lenker';
 
 const ManglerRettigheterIAltinnSide: React.FunctionComponent = () => {
     return (
         <div className="mangler-rettigheter-i-altinn__wrapper">
             <div className="mangler-rettigheter-i-altinn">
-                <PanelBase className="mangler-rettigheter-i-altinn-panel">
-                    <div className="mangler-rettigheter-i-altinn-panel__tekst-wrapper">
-                        <div className="mangler-rettigheter-i-altinn-panel__tekst_og_ikon">
-                            <img src={informasjonsirkelSvg}
-                                 className="mangler-rettigheter-i-altinn-panel__tekst_og_ikon__ikon" alt=""/>
-                            <Systemtittel tag="h2">
-                                Du mangler rettigheter i Altinn
-                            </Systemtittel>
-                        </div>
+                <div className="mangler-rettigheter-i-altinn__tekst_og_ikon">
+                    <img
+                        src={informasjonsirkelSvg}
+                        className="mangler-rettigheter-i-altinn__tekst_og_ikon__ikon"
+                        alt=""
+                    />
+                    <Systemtittel tag="h2">Du mangler rettigheter i Altinn</Systemtittel>
+                </div>
 
-                        <Normaltekst className="mangler-rettigheter-i-altinn-panel__overskrift">
-                            Du har ikke Altinn-tilgangen for å se sykefraværsstatistikk for denne virksomheten.
+                <Normaltekst className="mangler-rettigheter-i-altinn__overskrift">
+                    Du har ikke Altinn-tilgangen for å se sykefraværsstatistikk for denne
+                    virksomheten.
+                </Normaltekst>
+                <Element className="mangler-rettigheter-i-altinn__overskrift">
+                    For å se sykefraværsstatistikk kan du
+                </Element>
+                <ul className="mangler-rettigheter-i-altinn__list-wrapper">
+                    <li>
+                        <Normaltekst>
+                            Bytte til en virksomhet der du har tilgang til sykefraværsstatistikk
                         </Normaltekst>
-                        <Element className="mangler-rettigheter-i-altinn-panel__overskrift">
-                            For å se sykefraværsstatistikk kan du
-                        </Element>
-                        <ul className="mangler-rettigheter-i-altinn-panel__list-wrapper">
-                            <li>
-                                <Normaltekst>
-                                    Bytte til en virksomhet der du har tilgang til sykefraværsstatistikk
-                                </Normaltekst>
-                            </li>
-                            <li>
-                                <Normaltekst>
-                                    Be om tilgang til Altinn for denne virksomhet
-                                </Normaltekst>
-                            </li>
-                        </ul>
-                        <Lenke className="mangler-rettigheter-i-altinn-panel__lenke"
-                               href={'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/informasjon-om-tilgangsstyring'}>
-                            Les mer om hvordan tilgangsstyringen i Altinn fungerer
-                        </Lenke>
-                    </div>
-                </PanelBase>
+                    </li>
+                    <li>
+                        <Normaltekst>Be om tilgang til Altinn for denne virksomhet</Normaltekst>
+                    </li>
+                </ul>
+                <Lenke
+                    className="mangler-rettigheter-i-altinn__lenke"
+                    href={
+                        'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/informasjon-om-tilgangsstyring'
+                    }
+                >
+                    Les mer om hvordan tilgangsstyringen i Altinn fungerer
+                </Lenke>
             </div>
         </div>
     );
--- src/mocking/mock.ts
+++ src/mocking/mock.ts
@@ -2,9 +2,10 @@ import fetchMock from 'fetch-mock';
 import sykefravarsprosent from './sammenligning.json';
 import organisasjoner from './organisasjoner.json';
 
-const mockSammenligning = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
-//const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner);
-const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', 401);
+//const mockSammenligning = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', sykefravarsprosent);
+const mockSammenligning = () => fetchMock.get('express:/sykefravarsstatistikk/api/:orgnr/sammenligning', 403);
+const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', organisasjoner);
+//const mockMinSideArbeidsgiver = () => fetchMock.get('/min-side-arbeidsgiver/api/organisasjoner', 401);
 
 if (process.env.REACT_APP_MOCK_ALL) {
     mockSammenligning();
