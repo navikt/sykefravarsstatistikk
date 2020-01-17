1a7c448485174fe7d368ab47375634dc3acff518
MalazAlkoj <malaz.alkoj@nav.no>
2020-01-17 15:57:07 +0100

    første kalkulator komponent, bare input

--- src/Kostnadskalkulator/Kalkulator.tsx
+++ src/Kostnadskalkulator/Kalkulator.tsx
@@ -1,9 +1,30 @@
 import React, { FunctionComponent } from 'react';
-import { Systemtittel } from 'nav-frontend-typografi';
-import './Kalkulator.less'
+import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import './Kalkulator.less';
+import LesMerPanel from '../Forside/Legemeldtsykefraværpanel/HvordanBeregnesTallene/LesMerPanel/LesMerPanel';
+import { Input } from 'nav-frontend-skjema';
+
 const Kalkulator: FunctionComponent = () => (
-    <div>
-        <Systemtittel >Kostnadskalkulator</Systemtittel>
+    <div className="kalkulator__wrapper">
+        <div className="kalkulator">
+            <Systemtittel className="kalkulator__tittel">Kostnadskalkulator</Systemtittel>
+            <Normaltekst>
+                En catchy ingress dersom det er behov for det. En catchy ingress dersom det er behov for det. Den bør
+                helst ikke være så lang.
+            </Normaltekst>
+            <LesMerPanel åpneLabel="Slik beregnes tallene" lukkLabel="Lukk">
+                <Normaltekst>
+                    En catchy ingress dersom det er behov for det. En catchy ingress dersom det er behov for det. Den
+                    bør helst ikke være så lang.
+                </Normaltekst>
+            </LesMerPanel>
+
+            <Normaltekst>Deres tapte dagsverk siste 12 mnd: 1230</Normaltekst>
+            <Input label={<Element>Antall tapte dagsverk</Element>}></Input>
+
+            <Input label={<Element>Kostnad per dagsverk</Element>}></Input>
+
+        </div>
     </div>
 );
-export  default Kalkulator;
+export default Kalkulator;
