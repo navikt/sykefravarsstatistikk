7bb43e69c08437e90d1829213846fcc0416ad6b8
MalazAlkoj <malaz.alkoj@nav.no>
2020-01-20 13:43:18 +0100

    rydde opp

--- src/Kostnadskalkulator/Kalkulator.tsx
+++ src/Kostnadskalkulator/Kalkulator.tsx
@@ -6,10 +6,12 @@ import { Input } from 'nav-frontend-skjema';
 
 const Kalkulator: FunctionComponent = () => {
     const [tapteDagsverk, setTapteDagsverk] = useState<number>();
-    const [kostnadDagsverk,setKostnadDagsverk]=useState<number>();
+    const [kostnadDagsverk, setKostnadDagsverk] = useState<number>();
 
-    const setStateHvisValueErTall = (event: React.ChangeEvent<HTMLInputElement>, setState: (tall: number | undefined) => void) => {
-
+    const setStateHvisValueErTall = (
+        event: React.ChangeEvent<HTMLInputElement>,
+        setState: (tall: number | undefined) => void
+    ) => {
         const value = event.target.value;
         if (parseInt(value) > 0) {
             setState(parseInt(value));
@@ -18,28 +20,41 @@ const Kalkulator: FunctionComponent = () => {
         }
     };
 
-    return(
-    <div className="kalkulator__wrapper">
-        <div className="kalkulator">
-            <Systemtittel className="kalkulator__tittel">Kostnadskalkulator</Systemtittel>
-            <Normaltekst>
-                En catchy ingress dersom det er behov for det. En catchy ingress dersom det er behov for det. Den bør
-                helst ikke være så lang.
-            </Normaltekst>
-            <LesMerPanel åpneLabel="Slik beregnes tallene" lukkLabel="Lukk">
+    return (
+        <div className="kalkulator__wrapper">
+            <div className="kalkulator">
+                <Systemtittel className="kalkulator__tittel">Kostnadskalkulator</Systemtittel>
                 <Normaltekst>
-                    En catchy ingress dersom det er behov for det. En catchy ingress dersom det er behov for det. Den
-                    bør helst ikke være så lang.
+                    En catchy ingress dersom det er behov for det. En catchy ingress dersom det er
+                    behov for det. Den bør helst ikke være så lang.
                 </Normaltekst>
-            </LesMerPanel>
+                <LesMerPanel åpneLabel="Slik beregnes tallene" lukkLabel="Lukk">
+                    <Normaltekst>
+                        En catchy ingress dersom det er behov for det. En catchy ingress dersom det
+                        er behov for det. Den bør helst ikke være så lang.
+                    </Normaltekst>
+                </LesMerPanel>
 
-            <Normaltekst>Deres tapte dagsverk siste 12 mnd: 1230</Normaltekst>
+                <Normaltekst>Deres tapte dagsverk siste 12 mnd: 1230</Normaltekst>
 
-
-            <Input label={<Element>Kostnad per dagsverk</Element>} onChange={event => setStateHvisValueErTall(event, setKostnadDagsverk)} value={kostnadDagsverk || ''}/>
-            <Input label={<Element>Antall tapte dagsverk</Element>} onChange={event => setStateHvisValueErTall(event, setTapteDagsverk)} value={tapteDagsverk || ''}/>
-            <Normaltekst>Din kostnad er: {tapteDagsverk&&kostnadDagsverk? tapteDagsverk*kostnadDagsverk:''}</Normaltekst>
+                <Input
+                    label={<Element>Kostnad per dagsverk</Element>}
+                    onChange={event => setStateHvisValueErTall(event, setKostnadDagsverk)}
+                    value={kostnadDagsverk || ''}
+                    bredde={'L'}
+                />
+                <Input
+                    label={<Element>Antall tapte dagsverk</Element>}
+                    onChange={event => setStateHvisValueErTall(event, setTapteDagsverk)}
+                    value={tapteDagsverk || ''}
+                    bredde={'L'}
+                />
+                <Normaltekst>
+                    Din kostnad er:{' '}
+                    {tapteDagsverk && kostnadDagsverk ? tapteDagsverk * kostnadDagsverk : ''}
+                </Normaltekst>
+            </div>
         </div>
-    </div>)
+    );
 };
 export default Kalkulator;
