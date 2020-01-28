40c3eade76cee0cb433fb8b7092ca337822283e0
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-01-28 10:02:01 +0100

    Fjern unødvendig komponent

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import Banner from './Banner/Banner';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { OrganisasjonstreProvider } from './OrganisasjonstreProvider/OrganisasjonstreProvider';
 import { BASE_PATH } from './server/konstanter';
-import ForsideEllerFeilside from './ForsideEllerFeil/ForsideEllerFeilside';
 import { useRestTapteDagsverk } from './api/tapteDagsverk';
 import { useOrgnr } from './utils/orgnr-hook';
 import Kalkulator from './Kalkulator/Kalkulator';
 import { useRestSammenligning } from './api/sammenligning';
-import Forside from "./Forside/Forside";
-import Infopanel from "./Forside/Infopanel/Infopanel";
-import LegemeldtSykefraværPanel from "./Forside/Legemeldtsykefraværpanel/LegemeldtSykefraværPanel";
-import IAwebpanel from "./Forside/IAwebpanel/IAwebpanel";
+import Forside from './Forside/Forside';
+import Infopanel from './Forside/Infopanel/Infopanel';
+import LegemeldtSykefraværPanel from './Forside/Legemeldtsykefraværpanel/LegemeldtSykefraværPanel';
+import IAwebpanel from './Forside/IAwebpanel/IAwebpanel';
 
 export const PATH_FORSIDE = '/';
 export const PATH_KALKULATOR = '/kalkulator';
@@ -34,13 +33,11 @@ const AppContent: FunctionComponent = () => {
         <OrganisasjonstreProvider>
             <Banner tekst="Sykefraværsstatistikk" />
             <Route path={PATH_FORSIDE} exact={true}>
-                <ForsideEllerFeilside restSammenligning={restSammenligning}>
-                    <Forside>
-                        <Infopanel />
-                        <LegemeldtSykefraværPanel restSammenligning={restSammenligning}/>
-                        <IAwebpanel />
-                    </Forside>
-                </ForsideEllerFeilside>
+                <Forside restSammenligning={restSammenligning}>
+                    <Infopanel />
+                    <LegemeldtSykefraværPanel restSammenligning={restSammenligning} />
+                    <IAwebpanel />
+                </Forside>
             </Route>
             <Route path={PATH_KALKULATOR} exact={true}>
                 <Kalkulator defaultTapteDagsverk={restTapteDagsverk} />
--- src/Forside/Forside.tsx
+++ src/Forside/Forside.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import './Forside.less';
+import { RestSammenligning, RestSammenligningStatus } from '../api/sammenligning';
+import ManglerRettigheterIAltinnSide from '../FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide';
+import IkkeInnloggetSide from '../FeilSider/IkkeInnloggetSide/IkkeInnloggetSide';
 
-const Forside: React.FunctionComponent = props => {
-    return (
-        <div className="forside__wrapper">
-            <div className="forside">{props.children}</div>
-        </div>
-    );
+interface Props {
+    restSammenligning: RestSammenligning;
+}
+
+const Forside: React.FunctionComponent<Props> = props => {
+    switch (props.restSammenligning.status) {
+        case RestSammenligningStatus.HarIkkeRettigheterIAltinn: {
+            return <ManglerRettigheterIAltinnSide />;
+        }
+        case RestSammenligningStatus.IkkeInnlogget: {
+            return <IkkeInnloggetSide />;
+        }
+        default: {
+            return (
+                <div className="forside__wrapper">
+                    <div className="forside">{props.children}</div>
+                </div>
+            );
+        }
+    }
 };
 
 export default Forside;
