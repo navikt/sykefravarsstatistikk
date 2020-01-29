50f33df6e089d222299c627ad87e86e2cb6422ac
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-01-29 12:49:08 +0100

    Visuelle justeringer for kalkulator

---
+++ src/felleskomponenter/EksternLenke/EksternLenke.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+import Lenke, { Props } from 'nav-frontend-lenker';
+import { ReactComponent as EksternLenkeIkon } from './EksternLenkeIkon.svg';
+import "./EksternLenke.less";
+
+const EksternLenke: FunctionComponent<Props> = props => (
+    <Lenke {...props}>
+        {props.children}
+        <EksternLenkeIkon className="ekstern-lenke__ikon"/>
+    </Lenke>
+);
+
+export default EksternLenke;
