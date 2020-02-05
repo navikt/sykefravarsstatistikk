38078c73d68add5d4f6bb99cdb71a51b17b1c36a
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-02-05 16:04:26 +0100

    test

--- src/Graf/Graf.tsx
+++ src/Graf/Graf.tsx
@@ -11,7 +11,6 @@ import {
     YAxis,
 } from 'recharts';
 
-
 import './Graf.less';
 import Diamant from './Diamant';
 import Firkant from './Firkant';
@@ -19,31 +18,150 @@ import Firkant from './Firkant';
 const rnd = (min: number, max: number) => Math.random() * (max - min) + min;
 
 const sykefraværData = [
-    { name: '2015, 1. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2015, 2. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2015, 3. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(3, 6), sektor: rnd(4, 7)},
-    { name: '2015, 4. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
+    {
+        name: '2015, 1. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2015, 2. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2015, 3. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(3, 6),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2015, 4. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
 
-    { name: '2016, 1. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2016, 2. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2016, 3. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(3, 6), sektor: rnd(4, 7)},
-    { name: '2016, 4. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
+    {
+        name: '2016, 1. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2016, 2. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2016, 3. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(3, 6),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2016, 4. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
 
-    { name: '2017, 1. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2017, 2. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2017, 3. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(3, 6), sektor: rnd(4, 7)},
-    { name: '2017, 4. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
+    {
+        name: '2017, 1. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2017, 2. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2017, 3. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(3, 6),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2017, 4. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
 
-    { name: '2018, 1. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2018, 2. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2018, 3. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(3, 6), sektor: rnd(4, 7)},
-    { name: '2018, 4. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
+    {
+        name: '2018, 1. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2018, 2. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2018, 3. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(3, 6),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2018, 4. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
 
-    { name: '2019, 1. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2019, 2. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-    { name: '2019, 3. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(3, 6), sektor: rnd(4, 7)},
-    { name: '2019, 4. kvartal', virksomhet: rnd(5, 10), næring: rnd(4, 6), land: rnd(4, 5), sektor: rnd(4, 7)},
-
+    {
+        name: '2019, 1. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2019, 2. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2019, 3. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(3, 6),
+        sektor: rnd(4, 7),
+    },
+    {
+        name: '2019, 4. kvartal',
+        virksomhet: rnd(5, 10),
+        næring: rnd(4, 6),
+        land: rnd(4, 5),
+        sektor: rnd(4, 7),
+    },
 ];
 
 let maks = 0;
@@ -65,35 +183,6 @@ sykefraværData.forEach(data => {
 
 maks = Math.ceil(maks);
 
-/*
-const sykefraværData = [
-    { name: '2015, 1. kvartal', virksomhet: 5.5, næring: 7.6, land: 4.7, sektor: 6.5},
-    { name: '2015, 2. kvartal', virksomhet: 13.5, næring: 7.3, land: 4.8, sektor: 6.7},
-    { name: '2015, 3. kvartal', virksomhet: 4.2, næring: 5, land: 4.3, sektor: 5.4},
-    { name: '2015, 4. kvartal', virksomhet: 9.9, næring: 7, land: 4.4, sektor: 4.1},
-
-    { name: '2015, 1. kvartal', virksomhet: 5.5, næring: 7.6, land: 4.7, sektor: 6.5},
-    { name: '2015, 2. kvartal', virksomhet: 13.5, næring: 7.3, land: 4.8, sektor: 6.7},
-    { name: '2015, 3. kvartal', virksomhet: 4.2, næring: 5, land: 4.3, sektor: 5.4},
-    { name: '2015, 4. kvartal', virksomhet: 9.9, næring: 7, land: 4.4, sektor: 4.1},
-
-    { name: '2015, 1. kvartal', virksomhet: 5.5, næring: 7.6, land: 4.7, sektor: 6.5},
-    { name: '2015, 2. kvartal', virksomhet: 13.5, næring: 7.3, land: 4.8, sektor: 6.7},
-    { name: '2015, 3. kvartal', virksomhet: 4.2, næring: 5, land: 4.3, sektor: 5.4},
-    { name: '2015, 4. kvartal', virksomhet: 9.9, næring: 7, land: 4.4, sektor: 4.1},
-
-    { name: '2015, 1. kvartal', virksomhet: 5.5, næring: 7.6, land: 4.7, sektor: 6.5},
-    { name: '2015, 2. kvartal', virksomhet: 13.5, næring: 7.3, land: 4.8, sektor: 6.7},
-    { name: '2015, 3. kvartal', virksomhet: 4.2, næring: 5, land: 4.3, sektor: 5.4},
-    { name: '2015, 4. kvartal', virksomhet: 9.9, næring: 7, land: 4.4, sektor: 4.1},
-
-    { name: '2015, 1. kvartal', virksomhet: 5.5, næring: 7.6, land: 4.7, sektor: 6.5},
-    { name: '2015, 2. kvartal', virksomhet: 13.5, næring: 7.3, land: 4.8, sektor: 6.7},
-    { name: '2015, 3. kvartal', virksomhet: 4.2, næring: 5, land: 4.3, sektor: 5.4},
-    { name: '2015, 4. kvartal', virksomhet: 9.9, næring: 7, land: 4.4, sektor: 4.1},
-];
-
- */
 
 const margin = 50;
 const lineWidth = 2;
@@ -108,7 +197,7 @@ const Graf: FunctionComponent = () => {
                         data={sykefraværData}
                         margin={{ top: margin, right: margin, left: margin, bottom: margin }}
                     >
-                        <CartesianGrid strokeDasharray="3 3" stroke="#C6C2BF"/>
+                        <CartesianGrid strokeDasharray="3 3" stroke="#C6C2BF" />
                         <XAxis
                             dataKey="name"
                             height={30}
@@ -129,31 +218,33 @@ const Graf: FunctionComponent = () => {
                             //domain={[0, maks]}
                         />
                         <Tooltip />
-                        <Legend />
+                        <Legend wrapperStyle={{ paddingTop: 30 }}
+                                //formatter={(value, entry, index) => {console.log({value, entry, index})}}
+                        />
                         <Line
                             type="monotone"
                             dataKey="virksomhet"
                             stroke="#38A161" // grønn
                             strokeWidth={lineWidth}
-                            dot={<Symbols type="circle" size={dotSize} fill="#38A161"/>}
+                            dot={<Symbols type="circle" size={dotSize} fill="#38A161" />}
                         />
                         <Line
                             dataKey="næring"
                             stroke="#FF9100" // oransje
                             strokeWidth={lineWidth}
-                            dot={<Symbols type="diamond" size={dotSize} fill="#FF9100"/>}
+                            dot={<Symbols type="diamond" size={dotSize} fill="#FF9100" />}
                         />
                         <Line
                             dataKey="land"
                             stroke="#C30000" // rød
                             strokeWidth={lineWidth}
-                            dot={<Symbols type="square" size={dotSize} fill="#C30000"/>}
+                            dot={<Symbols type="square" size={dotSize} fill="#C30000" />}
                         />
                         <Line
                             dataKey="sektor"
                             stroke="#3385D1" // blå
                             strokeWidth={lineWidth}
-                            dot={<Symbols type="triangle" size={dotSize} fill="#3385D1"/>}
+                            dot={<Symbols type="triangle" size={dotSize} fill="#3385D1" />}
                         />
                     </LineChart>
                 </ResponsiveContainer>
