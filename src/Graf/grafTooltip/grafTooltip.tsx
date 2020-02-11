0ac3ed753e090d2e6fdcf2657c7ea9321f6a1f32
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-02-11 14:39:26 +0100

    cleanup

--- src/Graf/grafTooltip/grafTooltip.tsx
+++ src/Graf/grafTooltip/grafTooltip.tsx
@@ -21,7 +21,6 @@ const grafTooltip = () => (
         active={true}
         contentStyle={{ border: '2px solid #254B6D', borderRadius: '0.25rem' }}
         labelStyle={{ paddingBottom: '0.5rem' }}
-        //cursor={{strokeDasharray: 16, strokeWidth: 3, type: 'dot'}}
         cursor={{ stroke: '#254B6D\n', strokeWidth: 3, type: 'dot' }}
     />
 );
