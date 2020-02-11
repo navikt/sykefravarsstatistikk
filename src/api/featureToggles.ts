b8600182e28f2e6f086c353c5d822e40587ce5c8
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-02-11 14:04:26 +0100

    Gjør rest-objektet til toggles enklere

--- src/api/featureToggles.ts
+++ src/api/featureToggles.ts
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
-import { RestRessurs, RestStatus } from './api-utils';
+import { LasterInn, RestStatus, Suksess } from './api-utils';
 import { hentRestFeatureToggles } from './api';
 
 export type FeatureToggles = {
     [feature: string]: boolean;
 };
 
-export type RestFeatureToggles = RestRessurs<FeatureToggles>;
+export type RestFeatureToggles = LasterInn | Suksess<FeatureToggles>;
 
 export const useRestFeatureToggles = (): RestFeatureToggles => {
     const [featureToggles, setFeatureToggles] = useState<RestFeatureToggles>({
-        status: RestStatus.IkkeLastet,
+        status: RestStatus.LasterInn,
     });
 
     useEffect(() => {
         const hentFeatureTogglesOgSettState = async () => {
-            setFeatureToggles(await hentRestFeatureToggles('feature1', 'arbeidsgiver.test'));
+            setFeatureToggles(await hentRestFeatureToggles());
         };
         hentFeatureTogglesOgSettState();
     }, []);
