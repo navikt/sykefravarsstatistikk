fef894920230c5c30947aa637343a9bca048a081
MalazAlkoj <malaz.alkoj@nav.no>
2020-02-21 10:49:40 +0100

    sjekk om lenker åpnes i en ny fane

--- cypress/integration/videoerpanel.spec.js
+++ cypress/integration/videoerpanel.spec.js
@@ -3,12 +3,15 @@ context('VideoPanel', () => {
         cy.visit('http://localhost:3000/sykefravarsstatistikk');
     });
 
-    it("cy.go() - åpne eksterne lenke i nye faner", () => {
+    it('cy.go() - åpne eksterne lenke i nye faner', () => {
         cy.get('.videoerpanel__lenke')
             .contains('Redusering')
             .click();
         cy.get('.videoerpanel__lenke')
             .contains('Forebygge')
             .click();
+        cy.location().should(loc => {
+            expect(loc.pathname).to.eq('/sykefravarsstatistikk/');
+        });
     });
 });
