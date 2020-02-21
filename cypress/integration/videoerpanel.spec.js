a94a4a6514242aee7b2e442aa72b5e593ac17380
MalazAlkoj <malaz.alkoj@nav.no>
2020-02-21 09:21:01 +0100

    fjerne utkommenterte kode

--- cypress/integration/videoerpanel.spec.js
+++ cypress/integration/videoerpanel.spec.js
@@ -1,12 +1,9 @@
-/// <reference types="cypress" />
-
 context('VideoPanel', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/sykefravarsstatistikk');
     });
 
     it("cy.go() - åpne eksterne lenke i nye faner", () => {
-        // https://on.cypress.io/go
         cy.get('#reduseringAvSykefravar')
             .click();
         cy.get('#forebyggeArbeidsmiljoet')
