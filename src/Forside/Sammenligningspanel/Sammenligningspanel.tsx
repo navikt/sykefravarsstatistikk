dc567fd35b1b8065681d222205822cdd0ca2b0e2
MalazAlkoj <malaz.alkoj@nav.no>
2020-03-06 12:20:25 +0100

    før mering med master

--- src/Forside/Sammenligningspanel/Sammenligningspanel.tsx
+++ src/Forside/Sammenligningspanel/Sammenligningspanel.tsx
@@ -17,6 +17,12 @@ import SammenligningspanelFeilmelding from './SammenligningspanelFeilmelding';
 import NæringEllerBransjePanel from './NæringEllerBransjePanel/NæringEllerBransjePanel';
 import Virksomhetspanel from './Virksomhetspanel';
 
+import { ReactComponent as BedriftSvg } from './Bedrift.svg';
+import { ReactComponent as NorgeSvg } from './Norge.svg';
+import { ReactComponent as NæringSvg } from './Næring.svg';
+import { ReactComponent as SektorSvg } from './Sektor.svg';
+import { Innholdstittel, Systemtittel } from 'nav-frontend-typografi';
+
 interface Props {
     restSykefraværshistorikk: RestSykefraværshistorikk;
 }
@@ -48,7 +54,7 @@ const Sammenligningspanel: FunctionComponent<Props> = props => {
     const { årstall, kvartal } = sammenligningSisteKvartal;
 
     return (
-        <PanelBase className="sammenligningspanel">
+        /*<PanelBase className="sammenligningspanel">
             <div className="sammenligningspanel__tekst-wrapper">
                 <SammenligningspanelOverskrift
                     laster={laster}
@@ -87,7 +93,48 @@ const Sammenligningspanel: FunctionComponent<Props> = props => {
                 />
                 <HvordanBeregnesTallene />
             </div>
-        </PanelBase>
+        </PanelBase>*/
+        <div className="div-test typo-normal">
+            <PanelBase className="test-panel">
+                <Systemtittel className="infopanel__overskrift" tag="h2">
+                    <SammenligningspanelOverskrift
+                        laster={laster}
+                        className="sammenligningspanel__overskrift"
+                    >
+                        Legemeldt sykefravær i {kvartal}. kvartal {årstall}
+                    </SammenligningspanelOverskrift>
+                    <SammenligningspanelFeilmelding
+                        status={restStatus}
+                        className="sammenligningspanel__feilmelding"
+                    >
+                        Kunne ikke vise sykefraværet.
+                    </SammenligningspanelFeilmelding>                </Systemtittel>
+                <div className="test0">
+                    <div className="test1">
+                        <BedriftSvg />
+                        <Innholdstittel className="tittel1">13,1&nbsp;%</Innholdstittel>
+                        <div className="typo-element">Din virksomhet:</div>
+                        Flesk og fisk Oslo
+                    </div>
+                    <div className="test1">
+                        <NæringSvg />
+                        <Innholdstittel className="tittel1">6.2&nbsp;%</Innholdstittel>
+                        <div className="typo-element">Bransjen:</div>
+                        Barnehager
+                    </div>
+                    <div className="test1">
+                        <SektorSvg />
+                        <Innholdstittel className="tittel1">5,1&nbsp;%</Innholdstittel>
+                        <div className="typo-element">Offentlig sektor</div>
+                    </div>
+                    <div className="test1">
+                        <NorgeSvg />
+                        <Innholdstittel className="tittel1">4,1&nbsp;%</Innholdstittel>
+                        <div className="typo-element">Norge</div>
+                    </div>
+                </div>
+            </PanelBase>
+        </div>
     );
 };
 
