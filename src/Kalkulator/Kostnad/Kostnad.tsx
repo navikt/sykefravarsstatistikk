218c3466eabc5a67c3424a98333102e0025fc339
Malaz Alkoj <malaz.alkoj@nav.no>
2020-05-04 14:29:13 +0200

    ggjør design i kostnad til linjer,

--- src/Kalkulator/Kostnad/Kostnad.tsx
+++ src/Kalkulator/Kostnad/Kostnad.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Element, Sidetittel, Systemtittel } from 'nav-frontend-typografi';
+import { Element, Ingress, Sidetittel, Systemtittel, Undertittel } from 'nav-frontend-typografi';
 import { ReactComponent as SedlerIkon } from '../sedlerIkon.svg';
 import './Kostnad.less';
 
@@ -12,20 +12,25 @@ interface Props {
 const Kostnad: FunctionComponent<Props> = ({ nåværendeKostnad, ønsketKostnad, ønsketRedusert }) => (
     <div className="kostnad">
         <Systemtittel tag="h2" className="kostnad__tittel">
-            Kostnad <SedlerIkon className="kostnad__ikon" />
+            Resultat <SedlerIkon className="kostnad__ikon" />
         </Systemtittel>
-        <Element className="kostnad__tekst">
-            Totale kosnader per år med nåværende sykefravær
-        </Element>
-        <Sidetittel>{formaterTall(nåværendeKostnad)}&nbsp;kr</Sidetittel>
-        <Element className="kostnad__tekst">Totale kosnader per år med ønsket sykefravær </Element>
-        <Sidetittel>{formaterTall(ønsketKostnad)}&nbsp;kr</Sidetittel>
-        <Element className="kostnad__tekst">
-            {'Reduserers sykefraværet til ' +
-                ønsketRedusert +
-                ' dagsverk øker bunnlinjen årlig med'}
-        </Element>
-        <Sidetittel>{formaterTall(nåværendeKostnad - ønsketKostnad)}&nbsp;kr</Sidetittel>
+        <div className="kostnad__tekst">
+            <Element>Totale kosnader per år med nåværende sykefravær</Element>
+            <Element>{formaterTall(nåværendeKostnad)}&nbsp;kr</Element>
+        </div>
+        <div className="kostnad__tekst">
+            <Element>Totale kosnader per år med ønsket sykefravær </Element>
+            <Element>{formaterTall(ønsketKostnad)}&nbsp;kr</Element>
+        </div>
+
+        <div className="kostnad__tekst">
+            <Element>
+                {'Reduserers sykefraværet til ' +
+                    ønsketRedusert +
+                    ' dagsverk øker bunnlinjen årlig med'}
+            </Element>
+            <Element>{formaterTall(nåværendeKostnad - ønsketKostnad)}&nbsp;kr</Element>
+        </div>
     </div>
 );
 
