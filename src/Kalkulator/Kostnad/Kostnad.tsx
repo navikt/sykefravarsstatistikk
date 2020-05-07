972fa1ebf9df53d06cdf5d675c2fe14bd7bbfdc3
Malaz Alkoj <malaz.alkoj@nav.no>
2020-05-07 17:31:42 +0200

    bruk props

--- src/Kalkulator/Kostnad/Kostnad.tsx
+++ src/Kalkulator/Kostnad/Kostnad.tsx
@@ -12,56 +12,57 @@ interface Props {
     antallTapteDagsverkEllerProsent?: AntallTapteDagsverkEllerProsent;
 }
 
-const Kostnad: FunctionComponent<Props> = ({
-    nåværendeKostnad,
-    ønsketKostnad,
-    ønsketRedusert,
-    antallTapteDagsverkEllerProsent,
-}) => (
-    <div className="kostnad">
-        <Systemtittel tag="h2" className="kostnad__tittel">
-            Resultat <SedlerIkon className="kostnad__ikon" />
-        </Systemtittel>
-        <div className="kostnad__tekst">
-            <Element>Totale kostnader per år med nåværende sykefravær</Element>
-            <Element>{formaterTall(nåværendeKostnad)}&nbsp;kr</Element>
-        </div>
-        <div className={classNames('kostnad__tekst', 'kostnad__sisterad')}>
-            <Element>Totale kostnader per år med ønsket sykefravær </Element>
-            <Element>{formaterTall(ønsketKostnad)}&nbsp;kr</Element>
-        </div>
+const Kostnad: FunctionComponent<Props> = props => {
+    return (
+        <div className="kostnad">
+            <Systemtittel tag="h2" className="kostnad__tittel">
+                Resultat <SedlerIkon className="kostnad__ikon" />
+            </Systemtittel>
+            <div className="kostnad__tekst">
+                <Element>Totale kostnader per år med nåværende sykefravær</Element>
+                <Element>{formaterTall(props.nåværendeKostnad)}&nbsp;kr</Element>
+            </div>
+            <div className={classNames('kostnad__tekst', 'kostnad__sisterad')}>
+                <Element>Totale kostnader per år med ønsket sykefravær </Element>
+                <Element>{formaterTall(props.ønsketKostnad)}&nbsp;kr</Element>
+            </div>
 
-        <div className={classNames('kostnad__tekst', 'kostnad__resultatrad')}>
-            <Element>
-                {nåværendeKostnad - ønsketKostnad >= 0
-                    ? 'Reduserer sykefraværet til ' +
-                      (ønsketRedusert !== undefined ? ønsketRedusert.toFixed(2) : 0) +
-                      (antallTapteDagsverkEllerProsent ===
-                      AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT
-                          ? ' %'
-                          : ' dagsverk') +
-                      ' øker bunnlinjen årlig med'
-                    : 'Økes sykefraværet til ' +
-                      (ønsketRedusert !== undefined ? ønsketRedusert.toFixed(2) : 0) +
-                      (antallTapteDagsverkEllerProsent ===
-                      AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT
-                          ? ' %'
-                          : ' dagsverk') +
-                      ' taper dere ytterligere'}
-            </Element>
+            <div className={classNames('kostnad__tekst', 'kostnad__resultatrad')}>
+                <Element>
+                    {props.nåværendeKostnad - props.ønsketKostnad >= 0
+                        ? 'Reduserer sykefraværet til ' +
+                          (props.ønsketRedusert !== undefined
+                              ? props.ønsketRedusert.toFixed(2)
+                              : 0) +
+                          (props.antallTapteDagsverkEllerProsent ===
+                          AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT
+                              ? ' %'
+                              : ' dagsverk') +
+                          ' øker bunnlinjen årlig med'
+                        : 'Økes sykefraværet til ' +
+                          (props.ønsketRedusert !== undefined
+                              ? props.ønsketRedusert.toFixed(2)
+                              : 0) +
+                          (props.antallTapteDagsverkEllerProsent ===
+                          AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT
+                              ? ' %'
+                              : ' dagsverk') +
+                          ' taper dere ytterligere'}
+                </Element>
 
-            <Element
-                className={
-                    nåværendeKostnad - ønsketKostnad >= 0
-                        ? 'kostnad__sisteresultat'
-                        : 'kostnad__sisteresultat_minus'
-                }
-            >
-                {formaterTall(nåværendeKostnad - ønsketKostnad)}&nbsp;kr
-            </Element>
+                <Element
+                    className={
+                        props.nåværendeKostnad - props.ønsketKostnad >= 0
+                            ? 'kostnad__sisteresultat'
+                            : 'kostnad__sisteresultat_minus'
+                    }
+                >
+                    {formaterTall(props.nåværendeKostnad - props.ønsketKostnad)}&nbsp;kr
+                </Element>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 const formaterTall = (tall: number): string => {
     return tall.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
