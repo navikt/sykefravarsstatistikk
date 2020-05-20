7731d3260e0c4d96e85db078eadd7cf73ecb0527
Malaz Alkoj <malaz.alkoj@nav.no>
2020-05-20 09:01:44 +0200

    Varsel om ia web bli fjernet i i aweb panel (#81)
    * endre tekst og sett varselikon i IAWeb panel
    
    * IAweb lenke til EksternLenke

--- src/Forside/IAwebpanel/IAwebpanel.tsx
+++ src/Forside/IAwebpanel/IAwebpanel.tsx
@@ -5,6 +5,8 @@ import './IAwebpanel.less';
 import illustrasjon from './sort.svg';
 import { sendEvent } from '../../utils/metrikk-api';
 import amplitude from '../../utils/amplitude';
+import { ReactComponent as VarselIkon } from './varselIkon.svg';
+import EksternLenke from '../../felleskomponenter/EksternLenke/EksternLenke';
 
 const IAwebpanel: React.FunctionComponent = () => {
     return (
@@ -12,15 +14,18 @@ const IAwebpanel: React.FunctionComponent = () => {
             <img src={illustrasjon} alt="" className="iawebpanel__illustrasjon" />
             <div className="iawebpanel__tekst-wrapper">
                 <div className="iawebpanel__overskrift">
-                    <Systemtittel className="iawebpanel__overskrift" tag="h2">
-                        Se mer i Sykefraværsstatistikk for virksomheter (IA-web)
-                    </Systemtittel>
+                    <div className="iawebpanel__overskrift__header">
+                        <VarselIkon className="iawebpanel__overskrift__ikon" />
+                        <Systemtittel className="iawebpanel__overskrift" tag="h2">
+                            IA-web blir fjernet 5. juni 2020{' '}
+                        </Systemtittel>
+                    </div>
                     <Normaltekst>
                         Her kan du se flere detaljer om virksomhetens sykefravær, kostnadskalkulator
                         og tjenester som er mottatt fra NAV Arbeidslivssenter.
                     </Normaltekst>
                 </div>
-                <a
+                <EksternLenke
                     href="https://www.altinn.no/Pages/ServiceEngine/Start/StartService.aspx?ServiceEditionCode=2&ServiceCode=3403&Oselect=true&M=SP"
                     className="iawebpanel__lenke"
                     onClick={() => {
@@ -29,7 +34,7 @@ const IAwebpanel: React.FunctionComponent = () => {
                     }}
                 >
                     Gå til IA-web
-                </a>
+                </EksternLenke>
             </div>
         </PanelBase>
     );
