89a3f28872f8d0a8fef60036543e26a7f8ede5b5
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-06-03 08:54:56 +0200

    Legg på max-width på hjelpetekst + noreferrer noopener (#83)
    * Legg til noopener og noreferrer på eksterne lenker
    
    * Legg på maksbredde på hjelpetekst
    
    * Få bort warning fra css
    
    * Align tittel med ikon

--- src/Kalkulator/Kalkulator.tsx
+++ src/Kalkulator/Kalkulator.tsx
@@ -133,7 +133,9 @@ const Kalkulator: FunctionComponent<Props> = props => {
                 className="kalkulator__input"
             />
             <Hjelpetekst>
-                Ved fulltidsstilling regnes en hel stilling som ca 230 dagsverk per år
+                <Normaltekst className="kalkulator__hjelpetekst-innhold">
+                    Ved fulltidsstilling regnes en hel stilling som ca 230 dagsverk per år
+                </Normaltekst>
             </Hjelpetekst>
         </div>
     );
@@ -210,58 +212,58 @@ const Kalkulator: FunctionComponent<Props> = props => {
         restSykefraværshistorikk.status === RestStatus.Suksess &&
         skalViseDefaultTapteDagsverk &&
         antallTapteDagsverkEllerProsent !== AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT ? (
-            <>
-                <Hjelpetekst>
+            <Hjelpetekst>
+                <Normaltekst className="kalkulator__hjelpetekst-innhold">
                     Et dagsverk er arbeid som utføres på en dag. Antall tapte dagsverk bergenes ut
                     fra det legemeldte sykefraværet de siste 12 månedene og er tilgjengelig i NAVs
                     datagrunnlag.
-                </Hjelpetekst>
-            </>
+                </Normaltekst>
+            </Hjelpetekst>
         ) : skalViseDefaultTapteDagsverk ? (
-            <>
-                <Hjelpetekst>
+            <Hjelpetekst>
+                <Normaltekst className="kalkulator__hjelpetekst-innhold">
                     Sykefraværsprosenten regnes ut fra antall tapte dagsverk delt på antall mulige
                     dagsverk. Mulige dagsverk de siste 12 månedene er hentet fra det dere har meldt
                     inn i A-ordningen.
-                </Hjelpetekst>
-            </>
+                </Normaltekst>
+            </Hjelpetekst>
         ) : (
             antallTapteDagsverkEllerProsent !==
                 AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT && (
-                <>
-                    <Hjelpetekst>
+                <Hjelpetekst>
+                    <Normaltekst className="kalkulator__hjelpetekst-innhold">
                         Ved fulltidsstilling regnes en hel stilling som ca 230 dagsverk per år
-                    </Hjelpetekst>
-                </>
+                    </Normaltekst>
+                </Hjelpetekst>
             )
         );
     const ønsketTapteDagsverkSiste12Mnd =
         restSykefraværshistorikk.status === RestStatus.Suksess &&
         skalViseDefaultTapteDagsverk &&
         antallTapteDagsverkEllerProsent !== AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT ? (
-            <>
-                <Hjelpetekst>
+            <Hjelpetekst>
+                <Normaltekst className="kalkulator__hjelpetekst-innhold">
                     Et dagsverk er arbeid som utføres på en dag. Antall ønsket tapte dagsverk selv
                     velge for det ønskede legemeldte sykefraværet de siste 12 månedene for å beregne
                     hvor mye du kan spare.
-                </Hjelpetekst>
-            </>
+                </Normaltekst>
+            </Hjelpetekst>
         ) : skalViseDefaultTapteDagsverk ? (
-            <>
-                <Hjelpetekst>
+            <Hjelpetekst>
+                <Normaltekst className="kalkulator__hjelpetekst-innhold">
                     Ønsket sykefraværsprosent regnes ut fra ønsket antall tapte dagsverk delt på
                     antall mulige dagsverk dere har hatt de siste 12 månedene. Denne informasjonen
                     hentes fra det dere har meldt inn i A-ordningen.
-                </Hjelpetekst>
-            </>
+                </Normaltekst>
+            </Hjelpetekst>
         ) : (
             antallTapteDagsverkEllerProsent !==
                 AntallTapteDagsverkEllerProsent.SYKEFRAVÆRSPROSENT && (
-                <>
-                    <Hjelpetekst>
+                <Hjelpetekst>
+                    <Normaltekst className="kalkulator__hjelpetekst-innhold">
                         Ved fulltidsstilling regnes en hel stilling som ca 230 dagsverk per år
-                    </Hjelpetekst>
-                </>
+                    </Normaltekst>
+                </Hjelpetekst>
             )
         );
 
@@ -330,12 +332,14 @@ const Kalkulator: FunctionComponent<Props> = props => {
                             placeholder="kr"
                         />
                         <Hjelpetekst>
-                            Hvor mye taper virksomheten på at noen er sykemeldt en dag? I 2011
-                            beregnet SINTEF og NHO at hver uke med sykefravær koster en arbeidsgiver
-                            i snitt 13 000 kr. Det vil si 2600 kr per dag.{' '}
-                            <EksternLenke href="https://www.sintef.no/prosjekter/bedriftenes-kostnader-ved-sykefravar/">
-                                Les mer om hva som påvirker kostnader ved sykefravær.
-                            </EksternLenke>
+                            <Normaltekst className="kalkulator__hjelpetekst-innhold">
+                                Hvor mye taper virksomheten på at noen er sykemeldt en dag? I 2011
+                                beregnet SINTEF og NHO at hver uke med sykefravær koster en
+                                arbeidsgiver i snitt 13 000 kr. Det vil si 2600 kr per dag.{' '}
+                                <EksternLenke href="https://www.sintef.no/prosjekter/bedriftenes-kostnader-ved-sykefravar/">
+                                    Les mer om hva som påvirker kostnader ved sykefravær.
+                                </EksternLenke>
+                            </Normaltekst>
                         </Hjelpetekst>
                     </div>
                     {!skalViseDefaultTapteDagsverk &&
