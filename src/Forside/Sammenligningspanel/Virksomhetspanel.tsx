02182964b9639a3613112ebc931465d2948ad598
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-06-09 09:29:14 +0200

    tmp

--- src/Forside/Sammenligningspanel/Virksomhetspanel.tsx
+++ src/Forside/Sammenligningspanel/Virksomhetspanel.tsx
@@ -1,30 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import Sykefraværsprosentpanel, { SykefraværprosentpanelProps } from './Sykefraværsprosentpanel/Sykefraværsprosentpanel';
+import Sykefraværsprosentpanel, {
+    SykefraværprosentpanelProps,
+} from './Sykefraværsprosentpanel/Sykefraværsprosentpanel';
 import SammenligningsIkon from './SammenligningsIkon';
 import { SykefraværshistorikkType } from '../../api/sykefraværshistorikk';
 
-type Props = SykefraværprosentpanelProps & {
-    kvartal?: number;
-    årstall?: number;
-};
-
-const Virksomhetspanel: FunctionComponent<Props> = props => {
-    const {
-        sykefraværsprosent,
-        sykefraværprosentLabel,
-        årstall,
-        kvartal,
-        laster,
-        className,
-    } = props;
-
-    let feilmeldingHvisProsentErUndefined =
-        'Vi kan ikke vise informasjon om sykefraværet til virksomheten din.';
-    if (kvartal !== undefined && årstall !== undefined) {
-        feilmeldingHvisProsentErUndefined += ` Det kan være fordi det ikke er registrert sykefravær for virksomheten i ${kvartal}. kvartal ${årstall}.`;
-    }
-
-
+const Virksomhetspanel: FunctionComponent<SykefraværprosentpanelProps> = ({
+    sykefraværsprosent,
+    sykefraværprosentLabel,
+    laster,
+    className,
+}) => {
     return (
         <Sykefraværsprosentpanel
             sykefraværsprosent={sykefraværsprosent}
