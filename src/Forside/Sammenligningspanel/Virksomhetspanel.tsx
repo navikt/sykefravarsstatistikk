529be77124d3f19790397cf73c3b254e72ddae54
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-06-09 13:25:58 +0200

    Varsel for mindre virksomheter (#85)
    * Oppdater mocking
    
    * Legg på alertstripe
    
    * Trekk ut til egen komponent
    
    * legg til Kan ikke vises
    
    * tmp
    
    * Slett ubrukte filer
    
    * Trekk ut til komponent
    
    * Samle i komponent
    
    * Samle i komponent
    
    * Fjern ubrukt komponent

--- src/Forside/Sammenligningspanel/Virksomhetspanel.tsx
+++ src/Forside/Sammenligningspanel/Virksomhetspanel.tsx
@@ -1,43 +1,25 @@
 import React, { FunctionComponent } from 'react';
-import MaskertSykefraværprosentpanel from './MaskertSykefraværprosentpanel/MaskertSykefraværprosentpanel';
-import { SykefraværprosentpanelProps } from './Sykefraværsprosentpanel/Sykefraværsprosentpanel';
+import Sykefraværsprosentpanel, {
+    SykefraværprosentpanelProps,
+} from './Sykefraværsprosentpanel/Sykefraværsprosentpanel';
 import SammenligningsIkon from './SammenligningsIkon';
 import { SykefraværshistorikkType } from '../../api/sykefraværshistorikk';
 
-type Props = SykefraværprosentpanelProps & {
-    kvartal?: number;
-    årstall?: number;
-};
-
-const Virksomhetspanel: FunctionComponent<Props> = props => {
-    const {
-        sykefraværsprosent,
-        sykefraværprosentLabel,
-        årstall,
-        kvartal,
-        laster,
-        className,
-    } = props;
-
-    let feilmeldingHvisProsentErUndefined =
-        'Vi kan ikke vise informasjon om sykefraværet til virksomheten din.';
-    if (kvartal !== undefined && årstall !== undefined) {
-        feilmeldingHvisProsentErUndefined += ` Det kan være fordi det ikke er registrert sykefravær for virksomheten i ${kvartal}. kvartal ${årstall}.`;
-    }
-
+export const Virksomhetspanel: FunctionComponent<SykefraværprosentpanelProps> = ({
+    sykefraværsprosent,
+    sykefraværprosentLabel,
+    laster,
+    className,
+}) => {
     return (
-        <MaskertSykefraværprosentpanel
+        <Sykefraværsprosentpanel
             sykefraværsprosent={sykefraværsprosent}
             sykefraværprosentLabel={sykefraværprosentLabel}
-            labelHvisMaskert="Det er for få personer i datagrunnlaget til at vi kan vise sykefraværet."
-            labelHvisUndefined={feilmeldingHvisProsentErUndefined}
             laster={laster}
             ikon={<SammenligningsIkon label={SykefraværshistorikkType.VIRKSOMHET} />}
             className={className}
         >
             Din virksomhet:
-        </MaskertSykefraværprosentpanel>
+        </Sykefraværsprosentpanel>
     );
 };
-
-export default Virksomhetspanel;
