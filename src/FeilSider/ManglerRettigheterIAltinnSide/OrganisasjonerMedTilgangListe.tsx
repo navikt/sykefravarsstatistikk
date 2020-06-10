0a325fb7197f012e2262c833501a3103fe51e3b9
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-06-10 09:48:35 +0200

    Fikse margin/width

--- src/FeilSider/ManglerRettigheterIAltinnSide/OrganisasjonerMedTilgangListe.tsx
+++ src/FeilSider/ManglerRettigheterIAltinnSide/OrganisasjonerMedTilgangListe.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { RestAltinnOrganisasjoner } from '../../api/altinnorganisasjon-api';
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 
 interface Props {
     restOrganisasjonerForStatistikk: RestAltinnOrganisasjoner;
@@ -8,5 +9,9 @@ interface Props {
 export const OrganisasjonerMedTilgangListe: FunctionComponent<Props> = ({
     restOrganisasjonerForStatistikk,
 }) => {
-    return <div>hei</div>;
+    return (
+        <Ekspanderbartpanel border tittel="Disse virksomhetene har tilgang til Sykefraværsstatistikk">
+            hei
+        </Ekspanderbartpanel>
+    );
 };
