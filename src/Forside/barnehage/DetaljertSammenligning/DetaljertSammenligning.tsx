abebd25366d32709b6028a1531e02c46b3781139
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-25 09:46:24 +0200

    Flytt detaljerte paneler inn til komponenter

--- src/Forside/barnehage/DetaljertSammenligning/DetaljertSammenligning.tsx
+++ src/Forside/barnehage/DetaljertSammenligning/DetaljertSammenligning.tsx
@@ -1,51 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import Panel from 'nav-frontend-paneler';
 import './DetaljertSammenligning.less';
-import { Normaltekst } from 'nav-frontend-typografi';
-import LesMerPanel from '../../../felleskomponenter/LesMerPanel/LesMerPanel';
-import { Speedometer, SykefraværResultat } from '../Speedometer';
+import { SykefraværResultat } from '../Speedometer';
+import { KorttidSammenligningPanel } from './DetaljertSammenligningPanel/KorttidSammenligningPanel';
+import { LangtidSammenligningPanel } from './DetaljertSammenligningPanel/LangtidSammenligningPanel';
 
 export const DetaljertSammenligning: FunctionComponent = () => {
     return (
         <div className="detaljert-sammenligning">
-            <Panel className="detaljert-sammenligning__panel">
-                <Normaltekst tag="h3">
-                    Du har et <strong>lavere korttidsfravær</strong> enn bransjen
-                </Normaltekst>
-                <div className="detaljert-sammenligning__ikon-og-les-mer">
-                    <Speedometer resultat={SykefraværResultat.UNDER} />
-                    <LesMerPanel
-                        className="detaljert-sammenligning__les-mer"
-                        åpneLabel="Se tallene"
-                        lukkLabel="Lukk"
-                    >
-                        <div className="detaljert-sammenligning__les-mer-innhold">
-                            <Normaltekst>Andelen sykefraværsdager som er gradert:</Normaltekst>
-                            <Normaltekst>Ditt resultat: 20%</Normaltekst>
-                            <Normaltekst>Bransjens resultat: 5%</Normaltekst>
-                        </div>
-                    </LesMerPanel>
-                </div>
-            </Panel>
-            <Panel className="detaljert-sammenligning__panel">
-                <Normaltekst tag="h3">
-                    Du har et <strong>lavere korttidsfravær</strong> enn bransjen
-                </Normaltekst>
-                <div className="detaljert-sammenligning__ikon-og-les-mer">
-                    <Speedometer resultat={SykefraværResultat.OVER} />
-                    <LesMerPanel
-                        className="detaljert-sammenligning__les-mer"
-                        åpneLabel="Se tallene"
-                        lukkLabel="Lukk"
-                    >
-                        <div className="detaljert-sammenligning__les-mer-innhold">
-                            <Normaltekst>Andelen sykefraværsdager som er gradert:</Normaltekst>
-                            <Normaltekst>Ditt resultat: 20%</Normaltekst>
-                            <Normaltekst>Bransjens resultat: 5%</Normaltekst>
-                        </div>
-                    </LesMerPanel>
-                </div>
-            </Panel>
+            <KorttidSammenligningPanel resultat={SykefraværResultat.UNDER} />
+            <LangtidSammenligningPanel resultat={SykefraværResultat.OVER} />
         </div>
     );
 };
---
+++ src/Forside/barnehage/DetaljertSammenligning/DetaljertSammenligningPanel/KorttidSammenligningPanel.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { Speedometer, SykefraværResultat } from '../../Speedometer';
+import LesMerPanel from '../../../../felleskomponenter/LesMerPanel/LesMerPanel';
+import Panel from 'nav-frontend-paneler';
+import './DetaljertSammenligningPanel.less';
+
+interface Props {
+    resultat: SykefraværResultat;
+}
+export const KorttidSammenligningPanel: FunctionComponent<Props> = ({resultat}) => (
+    <Panel className="detaljert-sammenligning-panel">
+        <Normaltekst tag="h3">
+            Du har et <strong>lavere korttidsfravær</strong> enn bransjen
+        </Normaltekst>
+        <div className="detaljert-sammenligning-panel__ikon-og-les-mer">
+            <Speedometer resultat={resultat} />
+            <LesMerPanel
+                className="detaljert-sammenligning-panel__les-mer"
+                åpneLabel="Se tallene"
+                lukkLabel="Lukk"
+            >
+                <div className="detaljert-sammenligning-panel__les-mer-innhold">
+                    <Normaltekst>Andelen sykefraværsdager som er gradert:</Normaltekst>
+                    <Normaltekst>Ditt resultat: 20%</Normaltekst>
+                    <Normaltekst>Bransjens resultat: 5%</Normaltekst>
+                </div>
+            </LesMerPanel>
+        </div>
+    </Panel>
+);
