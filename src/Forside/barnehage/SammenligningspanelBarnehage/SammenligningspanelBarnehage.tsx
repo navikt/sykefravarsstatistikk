7f40aae825c58d965a8f6e58c72893dc0fa1bcf5
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-08-25 08:57:57 +0200

    Legg til react-print

--- src/Forside/barnehage/SammenligningspanelBarnehage/SammenligningspanelBarnehage.tsx
+++ src/Forside/barnehage/SammenligningspanelBarnehage/SammenligningspanelBarnehage.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent, useRef } from 'react';
 import './SammenligningspanelBarnehage.less';
 import { Knapp } from 'nav-frontend-knapper';
+import ReactToPrint from 'react-to-print';
 
 export const SammenligningspanelBarnehage: FunctionComponent = (props) => {
-    const panelRef = useRef();
+    const panelRef = useRef<HTMLDivElement>(null);
 
     return (
-        <div className="sammenligningspanel-barnehage">
-            <Knapp className="sammenligningspanel-barnehage__knapp">Skriv ut</Knapp>
+        <div className="sammenligningspanel-barnehage" ref={panelRef}>
+            <ReactToPrint
+                content={() => panelRef.current}
+                trigger={() => (
+                    <Knapp className="sammenligningspanel-barnehage__knapp">Skriv ut</Knapp>
+                )}
+            />
             {props.children}
         </div>
     );
