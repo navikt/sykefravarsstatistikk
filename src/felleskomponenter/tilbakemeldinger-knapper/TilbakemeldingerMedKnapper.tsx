e464d0b7f6b1c5a1dd781df35efec21db72d9dff
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-09-01 10:07:30 +0200

    Optimize import

--- src/felleskomponenter/tilbakemeldinger-knapper/TilbakemeldingerMedKnapper.tsx
+++ src/felleskomponenter/tilbakemeldinger-knapper/TilbakemeldingerMedKnapper.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useRef, useState } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Ingress, Undertekst } from 'nav-frontend-typografi';
 import { Knapp } from 'nav-frontend-knapper';
 import { useSendEvent } from '../../amplitude/amplitude';
@@ -9,16 +9,13 @@ interface Props {
 }
 export const TilbakemeldingerMedKnapper: FunctionComponent<Props> = (props) => {
     const sendEvent = useSendEvent();
-    //const harSendtTilbakeMelding = useRef(false);
     const [harSendtTilbakemeldingState, setharSendtTilbakemeldingState] = useState<boolean>(false);
     const knapper = (
         <>
             <Knapp
                 onClick={() => {
                     sendEvent('Lærte du noe nytt-' + props.område + '-JA', 'klikk');
-                    //harSendtTilbakeMelding.current = true;
                     setharSendtTilbakemeldingState(true);
-                    //console.log(harSendtTilbakeMelding.current);
                 }}
             >
                 JA
@@ -26,7 +23,6 @@ export const TilbakemeldingerMedKnapper: FunctionComponent<Props> = (props) => {
             <Knapp
                 onClick={() => {
                     sendEvent('Lærte du noe nytt-' + props.område + '-NEI', 'klikk');
-                    //harSendtTilbakeMelding.current = true;
                     setharSendtTilbakemeldingState(true);
                 }}
             >
