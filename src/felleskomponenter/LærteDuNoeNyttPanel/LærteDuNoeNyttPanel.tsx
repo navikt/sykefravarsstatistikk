881bbc10b6cf1b055613bc9899636dc5dbb1c821
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-09-02 11:16:20 +0200

    Refactor flytte logikk ut fra return statement

--- src/felleskomponenter/LærteDuNoeNyttPanel/LærteDuNoeNyttPanel.tsx
+++ src/felleskomponenter/LærteDuNoeNyttPanel/LærteDuNoeNyttPanel.tsx
@@ -15,55 +15,46 @@ const ANONYM_TILBAKEMELDING_INFOTEKST =
 export const LærteDuNoeNyttPanel: FunctionComponent<Props> = (props) => {
     const sendEvent = useSendEvent();
     const [harSendtTilbakemeldingState, setharSendtTilbakemeldingState] = useState<boolean>(false);
-    const knapper = (
-        <div className="lærte-du-noe-nytt-panel__knapp-wrapper">
-            <Knapp
-                className="lærte-du-noe-nytt-panel__knapp"
-                onClick={() => {
-                    sendEvent(props.område, 'klikk', { svar: 'ja' });
-                    setharSendtTilbakemeldingState(true);
-                }}
-            >
-                JA
-            </Knapp>
-            <Knapp
-                onClick={() => {
-                    sendEvent(props.område, 'klikk', { svar: 'nei' });
-                    setharSendtTilbakemeldingState(true);
-                }}
-            >
-                NEI
-            </Knapp>
-        </div>
-    );
 
-    return (
-        <div className="lærte-du-noe-nytt-panel">
-            {!harSendtTilbakemeldingState ? (
+    if (!harSendtTilbakemeldingState) {
+        return (
+            <div className="lærte-du-noe-nytt-panel">
                 <Element className="lærte-du-noe-nytt-panel__overskrift">{props.tekst}</Element>
-            ) : null}
 
-            {!harSendtTilbakemeldingState ? (
-                <>
-                    <Undertekst>
-                        Vi ønsker å forstå om sammenligningen ga deg ny kunnskap.
-                    </Undertekst>
-                    <Undertekst>{ANONYM_TILBAKEMELDING_INFOTEKST}</Undertekst>
-                </>
-            ) : null}
+                <Undertekst>Vi ønsker å forstå om sammenligningen ga deg ny kunnskap.</Undertekst>
+                <Undertekst>{ANONYM_TILBAKEMELDING_INFOTEKST}</Undertekst>
 
-            {!harSendtTilbakemeldingState ? (
-                knapper
-            ) : (
-                <>
-                    <Element className="lærte-du-noe-nytt-panel__overskrift">
-                        Takk for tilbakemeldingen din
-                    </Element>
-                    <Undertekst className="lærte-du-noe-nytt-panel__takk">
-                        {ANONYM_TILBAKEMELDING_INFOTEKST}
-                    </Undertekst>
-                </>
-            )}
-        </div>
-    );
+                <div className="lærte-du-noe-nytt-panel__knapp-wrapper">
+                    <Knapp
+                        className="lærte-du-noe-nytt-panel__knapp"
+                        onClick={() => {
+                            sendEvent(props.område, 'klikk', { svar: 'ja' });
+                            setharSendtTilbakemeldingState(true);
+                        }}
+                    >
+                        JA
+                    </Knapp>
+                    <Knapp
+                        onClick={() => {
+                            sendEvent(props.område, 'klikk', { svar: 'nei' });
+                            setharSendtTilbakemeldingState(true);
+                        }}
+                    >
+                        NEI
+                    </Knapp>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="lærte-du-noe-nytt-panel">
+                <Element className="lærte-du-noe-nytt-panel__overskrift">
+                    Takk for tilbakemeldingen din
+                </Element>
+                <Undertekst className="lærte-du-noe-nytt-panel__takk">
+                    {ANONYM_TILBAKEMELDING_INFOTEKST}
+                </Undertekst>
+            </div>
+        );
+    }
 };
