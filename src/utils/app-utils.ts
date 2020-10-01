0476343f0546512426db5a1d994b825e3090ad4e
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-10-01 16:52:10 +0200

    Prettier

--- src/utils/app-utils.ts
+++ src/utils/app-utils.ts
@@ -1,4 +1,4 @@
-export const sisteOppdatering = "02.09.2020";
+export const sisteOppdatering = '02.09.2020';
 
 export const formaterProsent = (prosent: number | null | undefined): string => {
     if (prosent === undefined || prosent === null) {
