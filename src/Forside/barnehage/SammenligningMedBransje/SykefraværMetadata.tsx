4304ecd693b772f90d26828387daeffcb934a961
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-10-07 15:25:02 +0200

    Diverse stylingjusteringer (#187)
    * Ta bort avstand mellom tall
    
    * Gjør metadata-tittel strong

--- src/Forside/barnehage/SammenligningMedBransje/SykefraværMetadata.tsx
+++ src/Forside/barnehage/SammenligningMedBransje/SykefraværMetadata.tsx
@@ -3,13 +3,19 @@ import { Ingress, Normaltekst } from 'nav-frontend-typografi';
 import { sisteOppdatering } from '../../../utils/app-utils';
 import './SykefraværMetadata.less';
 import { SammenligningsType } from '../vurderingstekster';
+import classNames from 'classnames';
 
 interface Props {
     sammenligningsType: SammenligningsType;
     periode: string;
+    className?: string;
 }
 
-export const SykefraværMetadata: FunctionComponent<Props> = ({ sammenligningsType, periode }) => {
+export const SykefraværMetadata: FunctionComponent<Props> = ({
+    sammenligningsType,
+    periode,
+    className,
+}) => {
     const getTittel = (sammenligningsType: SammenligningsType): ReactElement => {
         switch (sammenligningsType) {
             case SammenligningsType.TOTALT:
@@ -30,9 +36,9 @@ export const SykefraværMetadata: FunctionComponent<Props> = ({ sammenligningsTy
     };
 
     return (
-        <div className="sykefravær-metadata">
+        <div className={classNames('sykefravær-metadata', className)}>
             <Ingress tag="h3" className="sykefravær-metadata__tittel">
-                {getTittel(sammenligningsType)}
+                <strong>{getTittel(sammenligningsType)}</strong>
             </Ingress>
             <Normaltekst>Periode: {periode}</Normaltekst>
             <Normaltekst>Sist oppdatert: {sisteOppdatering}</Normaltekst>
