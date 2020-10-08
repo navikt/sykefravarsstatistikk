bf0e0558facccfd45385376ef1b855d2f6910f29
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-10-08 15:33:49 +0200

    Legg til komponent for barnehage-kurs

---
+++ src/Forside/barnehage/KursForBarnehager/KursForBarnehager.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react';
+import PanelBase from 'nav-frontend-paneler';
+import { Undertittel, Element } from 'nav-frontend-typografi';
+import EksternLenke from '../../../felleskomponenter/EksternLenke/EksternLenke';
+import kalenderSvg from './kalender.svg';
+import './KursForBarnehager.less';
+
+export const KursForBarnehager: FunctionComponent = () => (
+    <PanelBase className="kurs-for-barnehager">
+        <Element className="kurs-for-barnehager__nyhet">Nyhet</Element>
+        <div className="kurs-for-barnehager__tittel-wrapper">
+            <img src={kalenderSvg} alt="Kalenderikon" />
+            <Undertittel tag="h2">NAV har nettkurs spesielt rettet mot barnehager</Undertittel>
+        </div>
+        <EksternLenke href="https://arbeidsgiver.nav.no/kursoversikt/?tema=Inkluderende%20arbeidsliv%20(IA)">
+            Gå til kursoversikten
+        </EksternLenke>
+    </PanelBase>
+);
