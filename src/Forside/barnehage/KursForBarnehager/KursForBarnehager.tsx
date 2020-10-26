d08de3dae8720b39bde89d3d21ce810cd7eadb7b
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-10-26 14:18:03 +0100

    Fiks død lenke til kursoversikt

--- src/Forside/barnehage/KursForBarnehager/KursForBarnehager.tsx
+++ src/Forside/barnehage/KursForBarnehager/KursForBarnehager.tsx
@@ -1,18 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import PanelBase from 'nav-frontend-paneler';
-import { Undertittel, Element } from 'nav-frontend-typografi';
+import { Undertittel } from 'nav-frontend-typografi';
 import EksternLenke from '../../../felleskomponenter/EksternLenke/EksternLenke';
 import kalenderSvg from './kalender.svg';
 import './KursForBarnehager.less';
 
 export const KursForBarnehager: FunctionComponent = () => (
     <PanelBase className="kurs-for-barnehager">
-        <Element className="kurs-for-barnehager__nyhet">Nyhet</Element>
         <div className="kurs-for-barnehager__tittel-wrapper">
             <img src={kalenderSvg} alt="Kalenderikon" />
-            <Undertittel tag="h2">NAV har nettkurs spesielt rettet mot barnehager</Undertittel>
+            <Undertittel tag="h2">
+                NAV tilbyr nettkurs, med temaer som forebygging og oppfølging av sykefravær
+            </Undertittel>
         </div>
-        <EksternLenke href="https://arbeidsgiver.nav.no/kursoversikt/?tema=Inkluderende%20arbeidsliv%20(IA)&sokeord=barnehage">
+        <EksternLenke href="https://arbeidsgiver.nav.no/kursoversikt/?tema=Inkluderende%20arbeidsliv%20(IA)">
             Gå til kursoversikten
         </EksternLenke>
     </PanelBase>
