38b61325b4f3f89da322e1e1d4f3d90da089cab7
Lars Andreas Tveiten <36694755+LarsAndreasNav@users.noreply.github.com>
2020-10-27 16:06:46 +0100

    Ny ingress for barnehage-forside (#198)

--- src/Forside/barnehage/SammenligningIngress/SammenligningIngress.tsx
+++ src/Forside/barnehage/SammenligningIngress/SammenligningIngress.tsx
@@ -5,15 +5,19 @@ import KoronaInfotekst from '../../Sammenligningspanel/KoronaInfotekst/KoronaInf
 
 export const SammenligningIngress: FunctionComponent = () => {
     return (
-        <>
+        <div className="sammenligning-ingress">
             <KoronaInfotekst className="sammenligning-ingress__koronainfo" />
             <Systemtittel tag="h2" className="sammenligning-ingress__tittel">
                 Hvor er ditt potensial?
             </Systemtittel>
             <Normaltekst>
                 Du kan få hjelp til å forstå det ved å sammenligne deg med andre barnehager i Norge.
+                Vi har laget en oversikt for deg.
             </Normaltekst>
-            <Normaltekst>Vi har laget en oversikt for deg.</Normaltekst>
-        </>
+            <Normaltekst className="sammenligning-ingress__tilbakemelding-tekst">
+                Vi bruker tilbakemeldingsfunksjoner på siden for å gjøre tjenestene bedre.
+                Tilbakemeldingene kan ikke knyttes til deg eller din virksomhet.
+            </Normaltekst>
+        </div>
     );
 };
