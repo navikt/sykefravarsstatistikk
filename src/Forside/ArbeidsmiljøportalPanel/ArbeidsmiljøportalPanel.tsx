307c2df6b408be4baa9d444c3e79c5c15a3041e5
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-11-02 16:28:57 +0100

    Style komponenten

--- src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
+++ src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
@@ -3,25 +3,42 @@ import './ArbeidsmiljøportalPanel.less';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { Nyhet } from '../../felleskomponenter/Nyhet/Nyhet';
 import EksternLenke from '../../felleskomponenter/EksternLenke/EksternLenke';
+import arbeidsmiljøportalLogoSvg from './arbeidsmiljøportal-logo.svg';
 
 export const ArbeidsmiljøportalPanel: FunctionComponent = () => {
     return (
         <div className="arbeidsmiljøportal-panel">
-            <Nyhet />
-            <Systemtittel>Dette påvirker arbeidsmiljøet i næringsmiddelindustrien</Systemtittel>
-            <Normaltekst>
-                Arbeidsmiljøet handler om hvordan vi planlegger, organiserer og gjennomfører selve
-                jobben. For å forebygge arbeidsrelatert sykefravær, bør du vite hvor skoen trykker.
-            </Normaltekst>
-            <ul>
-                <li className="arbeidsmiljøportal-panel__listeelement">
-                    Se forebyggingspotensialet i næringsmiddelindustrien
-                </li>
-                <li className="arbeidsmiljøportal-panel__listeelement">
-                    Verktøy tilpasset din bransje
-                </li>
-            </ul>
-            <EksternLenke href="#">Gå til Arbeidsmiljøportalen</EksternLenke>
+            <div className="arbeidsmiljøportal-panel__tittel-wrapper">
+                <img
+                    src={arbeidsmiljøportalLogoSvg}
+                    className="arbeidsmiljøportal-panel__logo"
+                    alt="Logo for Arbeidsmiljøportalen"
+                />
+                <div>
+                    <Nyhet />
+                    <Systemtittel className="arbeidsmiljøportal-panel__tittel">
+                        Dette påvirker arbeidsmiljøet i næringsmiddelindustrien
+                    </Systemtittel>
+                </div>
+            </div>
+            <div className="arbeidsmiljøportal-panel__tekst-wrapper">
+                <Normaltekst className="arbeidsmiljøportal-panel__venstreblokk" >
+                    Arbeidsmiljøet handler om hvordan vi planlegger, organiserer og gjennomfører
+                    selve jobben. For å forebygge arbeidsrelatert sykefravær, bør du vite hvor skoen
+                    trykker.
+                </Normaltekst>
+                <div className="arbeidsmiljøportal-panel__høyreblokk">
+                    <ul className="arbeidsmiljøportal-panel__liste">
+                        <li className="arbeidsmiljøportal-panel__listeelement">
+                            Se forebyggingspotensialet i næringsmiddelindustrien
+                        </li>
+                        <li className="arbeidsmiljøportal-panel__listeelement">
+                            Verktøy tilpasset din bransje
+                        </li>
+                    </ul>
+                    <EksternLenke href="#">Gå til Arbeidsmiljøportalen</EksternLenke>
+                </div>
+            </div>
         </div>
     );
 };
