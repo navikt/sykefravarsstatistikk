f0c158231fc426095d70a356e8e649d05c35f6ea
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-11-02 15:57:16 +0100

    Legg til mer innhold i komponenten

--- src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
+++ src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
@@ -1,13 +1,27 @@
 import React, { FunctionComponent } from 'react';
 import './ArbeidsmiljøportalPanel.less';
-import { Systemtittel } from 'nav-frontend-typografi';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { Nyhet } from '../../felleskomponenter/Nyhet/Nyhet';
+import EksternLenke from '../../felleskomponenter/EksternLenke/EksternLenke';
 
 export const ArbeidsmiljøportalPanel: FunctionComponent = () => {
     return (
         <div className="arbeidsmiljøportal-panel">
             <Nyhet />
             <Systemtittel>Dette påvirker arbeidsmiljøet i næringsmiddelindustrien</Systemtittel>
+            <Normaltekst>
+                Arbeidsmiljøet handler om hvordan vi planlegger, organiserer og gjennomfører selve
+                jobben. For å forebygge arbeidsrelatert sykefravær, bør du vite hvor skoen trykker.
+            </Normaltekst>
+            <ul>
+                <li className="arbeidsmiljøportal-panel__listeelement">
+                    Se forebyggingspotensialet i næringsmiddelindustrien
+                </li>
+                <li className="arbeidsmiljøportal-panel__listeelement">
+                    Verktøy tilpasset din bransje
+                </li>
+            </ul>
+            <EksternLenke href="#">Gå til Arbeidsmiljøportalen</EksternLenke>
         </div>
     );
 };
