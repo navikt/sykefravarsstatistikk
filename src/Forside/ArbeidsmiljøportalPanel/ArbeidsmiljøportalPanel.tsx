f0d8d82ffdea87c35ba8e9aabf1ef836a111312e
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-11-02 15:53:12 +0100

    Legg til nyhetsmerke

--- src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
+++ src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import './ArbeidsmiljøportalPanel.less';
 import { Systemtittel } from 'nav-frontend-typografi';
+import { Nyhet } from '../../felleskomponenter/Nyhet/Nyhet';
 
 export const ArbeidsmiljøportalPanel: FunctionComponent = () => {
     return (
         <div className="arbeidsmiljøportal-panel">
+            <Nyhet />
             <Systemtittel>Dette påvirker arbeidsmiljøet i næringsmiddelindustrien</Systemtittel>
         </div>
     );
