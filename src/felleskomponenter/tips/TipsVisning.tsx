3b1a5982db8e4889bd8ec5114877d554af4dafe2
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-11-02 11:21:44 +0100

    Send med tipsID ved klikk på tips

--- src/felleskomponenter/tips/TipsVisning.tsx
+++ src/felleskomponenter/tips/TipsVisning.tsx
@@ -21,14 +21,16 @@ export const TipsVisning: FunctionComponent<Props> = ({ tips, className }) => {
             <div className="tips-visning__tekst-wrapper">
                 <div>
                     <Undertittel className="tips-visning__tittel" tag="p">
-                        <EksternLenke href={tips.href}>{tips.tittel}</EksternLenke>
+                        <EksternLenke href={tips.href} eventProperties={{ tipsID: tips.id }}>
+                            {tips.tittel}
+                        </EksternLenke>
                     </Undertittel>
                     <Normaltekst>{tips.ingress}</Normaltekst>
                 </div>
                 {tips.tidsbruk && (
                     <Tidsbruk className="tips-visning__tidsbruk">{tips.tidsbruk}</Tidsbruk>
                 )}
-                <VarDetteRelevant tipsID={tips.id}/>
+                <VarDetteRelevant tipsID={tips.id} />
             </div>
         </div>
     );
