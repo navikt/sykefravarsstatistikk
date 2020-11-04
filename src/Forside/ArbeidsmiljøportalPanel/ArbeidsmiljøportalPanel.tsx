b2728199865e6837257ef9da2972ec6b8e120626
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-11-04 09:45:28 +0100

    Legg på feature toggle for portalen

--- src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
+++ src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
@@ -1,11 +1,22 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import './ArbeidsmiljøportalPanel.less';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { Nyhet } from '../../felleskomponenter/Nyhet/Nyhet';
 import EksternLenke from '../../felleskomponenter/EksternLenke/EksternLenke';
 import arbeidsmiljøportalLogoSvg from './arbeidsmiljøportal-logo.svg';
+import { featureTogglesContext } from '../../utils/FeatureTogglesContext';
+import { RestFeatureToggles } from '../../api/featureToggles';
+import { RestStatus } from '../../api/api-utils';
 
 export const ArbeidsmiljøportalPanel: FunctionComponent = () => {
+    const restFeatureToggles = useContext<RestFeatureToggles>(featureTogglesContext);
+    if (
+        restFeatureToggles.status === RestStatus.LasterInn ||
+        !restFeatureToggles.data['sykefravarsstatistikk.arbeidsmiljøportal']
+    ) {
+        return null;
+    }
+
     return (
         <div className="arbeidsmiljøportal-panel">
             <div className="arbeidsmiljøportal-panel__tittel-wrapper">
@@ -15,14 +26,14 @@ export const ArbeidsmiljøportalPanel: FunctionComponent = () => {
                     alt="Logo for Arbeidsmiljøportalen"
                 />
                 <div className="arbeidsmiljøportal-panel__nyhet-og-tittel">
-                    <Nyhet className="arbeidsmiljøportal-panel__nyhet"/>
+                    <Nyhet className="arbeidsmiljøportal-panel__nyhet" />
                     <Systemtittel className="arbeidsmiljøportal-panel__tittel">
                         Dette påvirker arbeidsmiljøet i næringsmiddelindustrien
                     </Systemtittel>
                 </div>
             </div>
             <div className="arbeidsmiljøportal-panel__tekst-wrapper">
-                <Normaltekst className="arbeidsmiljøportal-panel__venstreblokk" >
+                <Normaltekst className="arbeidsmiljøportal-panel__venstreblokk">
                     Arbeidsmiljøet handler om hvordan vi planlegger, organiserer og gjennomfører
                     selve jobben. For å forebygge arbeidsrelatert sykefravær, bør du vite hvor skoen
                     trykker.
