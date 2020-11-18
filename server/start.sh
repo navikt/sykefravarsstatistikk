a55eabb45b416a7499e40d6b1b8fd214424f6c6f
Malaz Alkoj <malaz.alkoj@nav.no>
2020-11-18 15:29:44 +0100

    teste REACT_APP_MOCK i start.sh

--- server/start.sh
+++ server/start.sh
@@ -11,7 +11,7 @@ then
     done
 fi
 
-if [[ "$NAIS_CLUSTER_NAME" == "labs-gcp" ]];
+if [[ "$REACT_APP_MOCK" == "true" ]];
 then
   echo "Vi er inne i labs-gcp, kjører mock server [NAIS_CLUSTER_NAME=$NAIS_CLUSTER_NAME]"
   exec node herokuServer.js
