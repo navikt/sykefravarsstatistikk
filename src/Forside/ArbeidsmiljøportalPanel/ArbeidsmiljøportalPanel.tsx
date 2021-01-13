b83a75dfb6f7b48f8c30601998893a4178d3456a
Malaz Alkoj <malaz.alkoj@nav.no>
2021-01-13 12:44:47 +0100

    fjerne unødvendig div og less

--- src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
+++ src/Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel.tsx
@@ -29,11 +29,11 @@ export const ArbeidsmiljøportalPanel: FunctionComponent<Props> = ({ restVirksom
                     className="arbeidsmiljøportal-panel__logo"
                     alt="Logo for Arbeidsmiljøportalen"
                 />
-                <div className="arbeidsmiljøportal-panel__nyhet-og-tittel">
+
                     <Systemtittel className="arbeidsmiljøportal-panel__tittel">
                         Bedre arbeidsmiljø kan forebygge sykefravær
                     </Systemtittel>
-                </div>
+
             </div>
             <Normaltekst className="arbeidsmiljøportal-panel__tekst">
                 Bedre arbeidsmiljø styrker jobbnærværet, forebygger og reduserer sykefravær og
