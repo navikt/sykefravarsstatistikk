83820ab59fd2936e63e5d16f1e976735897d6455
Malaz Alkoj <malaz.alkoj@nav.no>
2021-08-03 14:29:19 +0200

    bruk ny endepunkt for kursoversikt, sfkurs

--- src/api/kurs-api.ts
+++ src/api/kurs-api.ts
@@ -3,6 +3,28 @@ import { getRestStatus, RestRessurs, RestStatus } from './api-utils';
 export const KURSOVERSIKT_API_PATH = '/kursoversikt/api/sfkurs';
 
 interface KursDto {
+    Title: string;
+    ShowInActivityList: number;
+    RegistrationUrl: string;
+    RegistrationToDateTime: string;
+    RegistrationPlaceName: string;
+    RegistrationID: string;
+    RegistrationFromDateTime: string;
+    RegistrationDeadline: string;
+    NumberOfParticipants: number;
+    MaxNumberOfParticipants: number;
+    FrontPageDescription: string;
+    Description: string;
+    configurable_custom: {
+        Underkategori: string | null;
+        Type: string;
+        Tema: string;
+        Fylke: string | null;
+    };
+    AvailableSeats: number;
+}
+
+/* {
     RegistrationID: number;
     Title: string;
     RegistrationUrl: string;
@@ -25,10 +47,10 @@ interface KursDto {
         'Type kurs': string;
         Tema: string;
     };
-}
+}*/
 
 export interface Kurs {
-    id: number;
+    id: string;
     tittel: string;
     tema?: string;
     type?: string;
@@ -43,7 +65,7 @@ const mapTilKurs = (kursDto: KursDto): Kurs => ({
     id: kursDto.RegistrationID,
     tittel: kursDto.Title,
     tema: kursDto.configurable_custom?.Tema,
-    type: kursDto.configurable_custom?.['Type kurs'],
+    type: kursDto.configurable_custom?.Type,
     start: new Date(kursDto.RegistrationFromDateTime),
     slutt: new Date(kursDto.RegistrationToDateTime),
     påmeldingsfrist: new Date(kursDto.RegistrationDeadline),
@@ -59,7 +81,7 @@ export const hentRestKurs = async (): Promise<RestKursliste> => {
             const kursliste: Kurs[] = ((await response.json()) as KursDto[]).map((kursDto) =>
                 mapTilKurs(kursDto)
             );
-
+            console.log('her er kurliste', kursliste);
             return {
                 status: RestStatus.Suksess,
                 data: kursliste,
