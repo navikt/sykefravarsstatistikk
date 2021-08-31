f543274dea00b16ee91af2541124ff9f8fc9862d
Malaz Alkoj <malaz.alkoj@nav.no>
2021-08-31 16:31:17 +0200

    sette riktig url til riktig miljø

--- src/microfrontends/microfrontends.tsx
+++ src/microfrontends/microfrontends.tsx
@@ -4,7 +4,8 @@ import NavFrontendSpinner from 'nav-frontend-spinner';
 import { AsyncNavspa } from '@navikt/navspa';
 import './microfrontends.less';
 import { SAMTALESTØTTE_MIKROFONTEND, SAMTALESTØTTE_MIKROFRONTEND_PROXY_PATH } from '../konstanter';
-import assetManifestParser, { createAssetManifestParser } from "./assetManifestUtils";
+import assetManifestParser, { createAssetManifestParser } from './assetManifestUtils';
+import { getMiljø } from '../utils/miljøUtils';
 
 type PodletProps = {
     visning: string | undefined;
@@ -15,10 +16,20 @@ const LasterInn: FunctionComponent = () => (
         <NavFrontendSpinner />
     </div>
 );
-
+const getAppBaseURLMikrofrontend = () => {
+    const miljø = getMiljø();
+    switch (miljø) {
+        case 'dev-sbs':
+            return 'https://arbeidsgiver-gcp.dev.nav.no/samtalestotte-podlet';
+        case 'local':
+            return 'http://localhost:3001/samtalestotte-podlet';
+        default:
+            return '/' + SAMTALESTØTTE_MIKROFONTEND;
+    }
+};
 const samtalestottePodletConfig = {
     appName: SAMTALESTØTTE_MIKROFONTEND,
-    appBaseUrl: 'http://localhost:3001/samtalestotte-podlet',
+    appBaseUrl: getAppBaseURLMikrofrontend(),
     assetManifestParser: createAssetManifestParser('http://localhost:3001/samtalestotte-podlet'),
     loader: <LasterInn />,
 };
