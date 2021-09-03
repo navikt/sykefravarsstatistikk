7af5d91487e666a92ecdae52cdabaf785c585ca9
Malaz Alkoj <malaz.alkoj@nav.no>
2021-09-03 12:45:46 +0200

    Lage tilpasset assetManifestParser som funker på tvers av domenene (#275)
    * Legge til ny proxy fil for å håndtere kall til mikro-frontend
    
    * WIP, prøver med å bruke proxy-path til mikrofrontend
    
    * Fjerne proxy
    
    * Deploy til dev-sbs og labs
    
    * Fiks proxy konfig til samtalestøtte
    
    * Aktivere proxy til Samtalestøtte i dev-sbs
    
    * Fiks dev-sbs deploy config
    
    * Fiks dev-sbs config: ikke tillatt med non-ascii chars for env variabler
    
    * Aktivere log=debug til feilsøking
    
    * bruk absolute path i assetManifestParser(vil bli erstattet i en ny versjon av Navspa)
    
    * sette riktig url til riktig miljø
    
    * Angi riktig url til AssetManifestParser
    
    * Fjerne console.log meldinger
    
    * sette labs miljø som default for test
    
    * sette sjekk avhengig av om de er i samme domene.
    
    * Refactor + prettier
    
    * fikse sårbarhet for tar
    
    * oppdatere sårbathet url-parse
    
    * oppdatere nav-dekoratoren
    
    * oppdatere sentry
    
    * oppdatere types/node
    
    * oppdatere react/ypes
    
    * oppdatere eslint parser
    
    * oppdatere browserlist, core-js
    
    * oppdatere craco-less
    
    * oppdatere  nav-frontend
    
    * oppdatere react-rounter-dom
    
    * oppdatere recharts
    
    * oppdatere typescript
    
    * oppdatere browserslist
    
    * oppdatere major: jsdom,glob-parent,@types/jest
    
    Co-authored-by: Thomas Dufourd <thomas.dufourd@gmail.com>

--- src/microfrontends/assetManifestUtils.ts
+++ src/microfrontends/assetManifestUtils.ts
@@ -1,20 +1,39 @@
-import { ManifestObject } from '@navikt/navspa/dist/async/async-navspa';
+import { AssetManifestParser, ManifestObject } from '@navikt/navspa/dist/async/async-navspa';
+import * as http from 'http';
+
+// TODO: koden i denne filen kommer i en senere versjon av NavSpa
+//  -- koden kan da slettes når @navikt/navspa til v4.x blir tilgjengelig
 
 export type AssetManifest = {
     files: Record<string, string>;
     entrypoints: string[];
 };
 
-const assetManifestParser = (manifestObject: ManifestObject): string[] => {
+export type MikrofrontendConfig = {
+    appBaseUrl: string;
+    isSameDomain: boolean;
+};
+
+/**
+ * Extracts paths to load from a Create React App asset manifest.
+ * @param manifestObject parsed json from the asset manifest
+ */
+function extractPathsFromCRAManifest(manifestObject: ManifestObject): string[] {
     const pathsToLoad: string[] = [];
 
-    const { files, entrypoints } = manifestObject as AssetManifest;
+    const { files, entrypoints } = manifestObject as {
+        files: { [name: string]: string };
+        entrypoints: string[];
+    };
 
     if (files == null || typeof files !== 'object' || !Array.isArray(entrypoints)) {
         throw new Error('Invalid manifest: ' + JSON.stringify(manifestObject));
     }
 
-    const fileList = Object.entries(files).map(([name, path]) => ({ name, path }));
+    const fileList = Object.entries(files).map(([name, path]) => ({ name, path })) as {
+        name: string;
+        path: string;
+    }[];
 
     entrypoints.forEach((entrypoint) => {
         const matchingFile = fileList.find((file) => file.path.endsWith(entrypoint));
@@ -26,12 +45,29 @@ const assetManifestParser = (manifestObject: ManifestObject): string[] => {
         }
     });
 
-    const environmentFile = fileList.find((file) => file.name === 'env.js');
-    if (environmentFile) {
-        pathsToLoad.push(environmentFile.path);
-    }
-
     return pathsToLoad;
-};
+}
 
-export default assetManifestParser;
+export function createAssetManifestParser(
+    mikrofrontendConfig: MikrofrontendConfig
+): AssetManifestParser {
+    return (manifestObject: ManifestObject) => {
+        const pathsToLoad = extractPathsFromCRAManifest(manifestObject);
+        const debugPaths = pathsToLoad.map((path) =>
+            makeAbsolute(mikrofrontendConfig.appBaseUrl, path, mikrofrontendConfig.isSameDomain)
+        );
+        return debugPaths;
+    };
+}
+
+export function makeAbsolute(
+    baseUrl: string,
+    maybeAbsolutePath: string,
+    isSameDomain: boolean
+): string {
+    if (maybeAbsolutePath.startsWith('http') || isSameDomain) {
+        return maybeAbsolutePath;
+    }
+    const url = new URL(baseUrl);
+    return `${url.origin}${maybeAbsolutePath}`;
+}
--- src/microfrontends/microfrontends.tsx
+++ src/microfrontends/microfrontends.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from 'react';
 
-import assetManifestParser from "./assetManifestUtils";
-import NavFrontendSpinner from "nav-frontend-spinner";
-import { AsyncNavspa } from "@navikt/navspa";
-import "./microfrontends.less";
+import NavFrontendSpinner from 'nav-frontend-spinner';
+import { AsyncNavspa } from '@navikt/navspa';
+import './microfrontends.less';
+import { SAMTALESTØTTE_MIKROFONTEND, SAMTALESTØTTE_MIKROFRONTEND_PATH } from '../konstanter';
+import { createAssetManifestParser, MikrofrontendConfig } from './assetManifestUtils';
+import { getMiljø } from '../utils/miljøUtils';
 
 type PodletProps = {
     visning: string | undefined;
@@ -15,12 +17,33 @@ const LasterInn: FunctionComponent = () => (
     </div>
 );
 
+const getMikrofrontendConfig = (): MikrofrontendConfig => {
+    const miljø = getMiljø();
+
+    switch (miljø) {
+        case 'dev-sbs':
+            return {
+                appBaseUrl:
+                    'https://arbeidsgiver-gcp.dev.nav.no' + SAMTALESTØTTE_MIKROFRONTEND_PATH,
+                isSameDomain: false,
+            };
+
+        case 'local':
+            return {
+                appBaseUrl: 'http://localhost:3001' + SAMTALESTØTTE_MIKROFRONTEND_PATH,
+                isSameDomain: false,
+            };
+
+        default:
+            return { appBaseUrl: SAMTALESTØTTE_MIKROFRONTEND_PATH, isSameDomain: true };
+    }
+};
+
 const samtalestottePodletConfig = {
-    appName: 'samtalestotte-podlet',
-    appBaseUrl: '/samtalestotte-podlet',
-    assetManifestParser,
+    appName: SAMTALESTØTTE_MIKROFONTEND,
+    appBaseUrl: getMikrofrontendConfig().appBaseUrl,
+    assetManifestParser: createAssetManifestParser(getMikrofrontendConfig()),
     loader: <LasterInn />,
 };
 
-export const SamtalestøttePodlet =
-    AsyncNavspa.importer<PodletProps>(samtalestottePodletConfig);
+export const SamtalestøttePodlet = AsyncNavspa.importer<PodletProps>(samtalestottePodletConfig);
