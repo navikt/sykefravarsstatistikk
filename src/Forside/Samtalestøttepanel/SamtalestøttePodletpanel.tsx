94d1dd756ce0b7dee407279d798bf57a23a6a69a
Malaz Alkoj <malaz.alkoj@nav.no>
2021-09-27 09:40:55 +0200

    Sende orgnr til podlet (#284)
    * sende orgnr som props til podlet
    
    * sende bransjen til preprod

--- src/Forside/Samtalestøttepanel/SamtalestøttePodletpanel.tsx
+++ src/Forside/Samtalestøttepanel/SamtalestøttePodletpanel.tsx
@@ -3,13 +3,15 @@ import "./Samtalestøttepanel.less";
 import "../../felleskomponenter/InternLenke/InternLenke.less";
 import { SamtalestøttePodlet } from "../../microfrontends/microfrontends";
 import PanelBase from "nav-frontend-paneler";
+import { useOrgnr } from "../../utils/orgnr-hook";
 
 const SamtalestøttePodletpanel: FunctionComponent = () => {
-
+const orgnr=useOrgnr();
     return (
         <PanelBase className="samtalestøttepanel">
             <SamtalestøttePodlet
                 visning='PANEL_MED_IKON_OG_TEKST'
+                orgnr={orgnr}
             />
         </PanelBase>
     );
