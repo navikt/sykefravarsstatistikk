1e6664b9284302c6a8f66cfe5d4a42600bde9360
runarskagestad <runar.skagestad@nav.no>
2021-10-26 14:17:59 +0200

    Byttet default-kalkulator til "Dagsverk". Fjernet en "kalkulator-ny"-wrapper

--- src/Kalkulator/Kalkulator/Kalkulator.tsx
+++ src/Kalkulator/Kalkulator/Kalkulator.tsx
@@ -17,7 +17,7 @@ interface Props {
 
 const Kalkulator: FunctionComponent<Props> = ({ restSykefraværshistorikk }) => {
     const [kalkulatorvariant, setKalkulatorvariant] = useState<Kalkulatorvariant>(
-        Kalkulatorvariant.Prosent,
+        Kalkulatorvariant.Dagsverk,
     );
     const orgnr = useOrgnr();
 
@@ -29,53 +29,51 @@ const Kalkulator: FunctionComponent<Props> = ({ restSykefraværshistorikk }) => 
     }, []);
 
     return (
-        <div className='kalkulator-ny'>
-            <div className='kalkulator__wrapper'>
-                <div className='kalkulator'>
-                    <div>
-                        <div className='kalkulator__tittel-wrapper'>
-                            <div>
-                                <Systemtittel tag='h1' className='kalkulator__tittel'>
-                                    Hvor mye koster sykefraværet?
-                                </Systemtittel>
-                                <Normaltekst className='kalkulator__ingress'>
-                                    Her kan du beregne hvor mye sykefraværet koster og hvor mye du
-                                    kan spare. Lønnskostnader og sykepengerefusjon er ikke med i
-                                    regnestykket og kommer i tillegg til kostnad per dag.
-                                </Normaltekst>
-                            </div>
-                            <div className='kalkulator__dagsverk-eller-prosent-toggle'>
-                                <ToggleKnappPure
-                                    pressed={kalkulatorvariant === Kalkulatorvariant.Prosent}
-                                    onClick={() => {
-                                        setKalkulatorvariant(Kalkulatorvariant.Prosent);
-                                    }}
-                                >
-                                    Prosent
-                                </ToggleKnappPure>
-                                <ToggleKnappPure
-                                    pressed={kalkulatorvariant === Kalkulatorvariant.Dagsverk}
-                                    onClick={() => {
-                                        setKalkulatorvariant(Kalkulatorvariant.Dagsverk);
-                                    }}
-                                >
-                                    Dagsverk
-                                </ToggleKnappPure>
-                            </div>
+        <div className='kalkulator__wrapper'>
+            <div className='kalkulator'>
+                <div>
+                    <div className='kalkulator__tittel-wrapper'>
+                        <div>
+                            <Systemtittel tag='h1' className='kalkulator__tittel'>
+                                Hvor mye koster sykefraværet?
+                            </Systemtittel>
+                            <Normaltekst className='kalkulator__ingress'>
+                                Her kan du beregne hvor mye sykefraværet koster og hvor mye du
+                                kan spare. Lønnskostnader og sykepengerefusjon er ikke med i
+                                regnestykket og kommer i tillegg til kostnad per dag.
+                            </Normaltekst>
                         </div>
-                        <Normaltekst className='kalkulator__input-overskrift'>
-                            Fyll inn og juster tallene så de passer for deg
-                        </Normaltekst>
-                        {kalkulatorvariant === Kalkulatorvariant.Dagsverk ? (
-                            <KalkulatorMedDagsverk
-                                restSykefraværshistorikk={restSykefraværshistorikk}
-                            />
-                        ) : (
-                            <KalkulatorMedProsent
-                                restSykefraværshistorikk={restSykefraværshistorikk}
-                            />
-                        )}
+                        <div className='kalkulator__dagsverk-eller-prosent-toggle'>
+                            <ToggleKnappPure
+                                pressed={kalkulatorvariant === Kalkulatorvariant.Prosent}
+                                onClick={() => {
+                                    setKalkulatorvariant(Kalkulatorvariant.Prosent);
+                                }}
+                            >
+                                Prosent
+                            </ToggleKnappPure>
+                            <ToggleKnappPure
+                                pressed={kalkulatorvariant === Kalkulatorvariant.Dagsverk}
+                                onClick={() => {
+                                    setKalkulatorvariant(Kalkulatorvariant.Dagsverk);
+                                }}
+                            >
+                                Dagsverk
+                            </ToggleKnappPure>
+                        </div>
                     </div>
+                    <Normaltekst className='kalkulator__input-overskrift'>
+                        Fyll inn og juster tallene så de passer for deg
+                    </Normaltekst>
+                    {kalkulatorvariant === Kalkulatorvariant.Dagsverk ? (
+                        <KalkulatorMedDagsverk
+                            restSykefraværshistorikk={restSykefraværshistorikk}
+                        />
+                    ) : (
+                        <KalkulatorMedProsent
+                            restSykefraværshistorikk={restSykefraværshistorikk}
+                        />
+                    )}
                 </div>
             </div>
         </div>
