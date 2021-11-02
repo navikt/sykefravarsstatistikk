1d133393380a2168c11cb3b8be43fc56726d3f47
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-11-02 08:39:39 +0100

    Oppgrader til navspa 4 (#299)
    * Oppgradere til NavSpa 4.0
    
    * Deploy til labs og dev-sbs

--- src/microfrontends/microfrontends.tsx
+++ src/microfrontends/microfrontends.tsx
@@ -4,12 +4,15 @@ import NavFrontendSpinner from 'nav-frontend-spinner';
 import { AsyncNavspa } from '@navikt/navspa';
 import './microfrontends.less';
 import { SAMTALESTØTTE_MIKROFONTEND, SAMTALESTØTTE_MIKROFRONTEND_PATH } from '../konstanter';
-import { createAssetManifestParser, MikrofrontendConfig } from './assetManifestUtils';
 import { getMiljø } from '../utils/miljøUtils';
 
 type PodletProps = {
     visning: string | undefined;
     orgnr?: string;
+};
+
+export type MikrofrontendConfig = {
+    appBaseUrl: string;
 };
 
 const LasterInn: FunctionComponent = () => (
@@ -41,7 +44,6 @@ const getMikrofrontendConfig = (): MikrofrontendConfig => {
 const samtalestottePodletConfig = {
     appName: SAMTALESTØTTE_MIKROFONTEND,
     appBaseUrl: getMikrofrontendConfig().appBaseUrl,
-    assetManifestParser: createAssetManifestParser(getMikrofrontendConfig()),
     loader: <LasterInn />,
 };
 
