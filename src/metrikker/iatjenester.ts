c5ead6a5c1192315d155d0ca8d9466a9aa50e5b6
runarskagestad <runar.skagestad@nav.no>
2021-11-10 07:29:16 +0100

    Fjernet ubrukt "ekstradata"-relatert kode

--- src/metrikker/iatjenester.ts
+++ src/metrikker/iatjenester.ts
@@ -1,51 +1,7 @@
-import { MutableRefObject, useContext, useEffect, useRef } from 'react';
-import { RestVirksomhetsdata, Virksomhetsdata } from '../api/virksomhetsdata-api';
-import { virksomhetsdataContext } from '../utils/virksomhetsdataContext';
-import { enhetsregisteretContext, EnhetsregisteretState } from '../utils/enhetsregisteretContext';
-import { RestStatus } from '../api/api-utils';
-import { mapTilNæringsbeskrivelse } from '../amplitude/næringsbeskrivelser';
-import {
-    InstitusjonellSektorkode,
-    OverordnetEnhet,
-    RestOverordnetEnhet,
-    RestUnderenhet,
-    Underenhet,
-} from '../api/enhetsregisteret-api';
+import { useContext, useEffect } from 'react';
 import { useOrgnr } from '../utils/orgnr-hook';
 import { tilIsoDatoMedUtcTimezoneUtenMillis } from '../utils/app-utils';
 import { iaTjenesterMetrikkerContext } from './IaTjenesterMetrikkerContext';
-
-interface IaTjenesteMetrikkerEkstraData {
-    orgnr: String;
-    bransje: string;
-    antallAnsatte: number;
-    næring2siffer: string;
-    næring2SifferBeskrivelse: string;
-    næringKode5Siffer: string;
-    næringskode5SifferBeskrivelse: String;
-    institusjonellSektorKode: InstitusjonellSektorkode;
-    fylkesnummer: String;
-    fylke: String;
-    kommunenummer: String;
-    kommune: String;
-}
-
-interface IatjenesteMetrikk {
-    orgnr: String;
-    næringKode5Siffer: String;
-    type: String;
-    kilde: String;
-    tjenesteMottakkelsesdato: String;
-    antallAnsatte: number;
-    næringskode5SifferBeskrivelse: String;
-    næring2SifferBeskrivelse: String;
-    ssbSektorKode: String;
-    ssbSektorKodeBeskrivelse: String;
-    fylkesnummer: String;
-    fylke: String;
-    kommunenummer: String;
-    kommune: String;
-}
 
 interface IaTjenesteMetrikkForenklet {
     orgnr: String;
@@ -102,30 +58,6 @@ function byggForenkletIaTjenesteMottattMetrikk(
     return iaTjenesteMetrikk;
 }
 
-// HVORFOR TRENGER VI BÅDE DENNE OG byggIaTjenesteMottattMetrikk ?
-function byggDirekteIaTjenesteMottattMetrikk(
-    nåværendeOrgnr: string | undefined,
-    ekstradata: IaTjenesteMetrikkerEkstraData,
-) {
-    const iaTjenesteMetrikk: IatjenesteMetrikk = {
-        orgnr: nåværendeOrgnr ?? '',
-        antallAnsatte: ekstradata.antallAnsatte ?? 0,
-        kilde: 'SYKEFRAVÆRSSTATISTIKK',
-        type: 'DIGITAL_IA_TJENESTE',
-        fylke: ekstradata.fylke ?? '',
-        fylkesnummer: ekstradata.fylkesnummer ?? '',
-        kommune: ekstradata.kommune ?? '',
-        kommunenummer: ekstradata.kommunenummer ?? '',
-        næring2SifferBeskrivelse: ekstradata.næring2SifferBeskrivelse ?? '',
-        næringKode5Siffer: ekstradata.næringKode5Siffer ?? '',
-        næringskode5SifferBeskrivelse: ekstradata.næringskode5SifferBeskrivelse ?? '',
-        ssbSektorKode: ekstradata.institusjonellSektorKode?.kode ?? '',
-        ssbSektorKodeBeskrivelse: ekstradata.institusjonellSektorKode?.beskrivelse ?? '',
-        tjenesteMottakkelsesdato: tilIsoDatoMedUtcTimezoneUtenMillis(new Date()),
-    };
-    return iaTjenesteMetrikk;
-}
-
 export const useSendIaTjenesteMetrikkEvent = (): (() => Promise<boolean>) => {
     const nåværendeOrgnr = useOrgnr();
     const iaTjenesteMetrikk = byggForenkletIaTjenesteMottattMetrikk(nåværendeOrgnr);
@@ -151,100 +83,9 @@ export const sendForenkletIATjenesteMetrikk = async (iatjeneste: IaTjenesteMetri
         console.log('Kall til ia-tjeneste-metrikker (', iaTjenesterMetrikkerAPI, ') gjennomført med data ', data);
         return data.status === 'created';
     } catch (e) {
-        console.log('kall til ia-tjeneste-metrikker feilet! ', e);
+        console.log('kall til ia-tjeneste-metrikker ', iaTjenesterMetrikkerAPI, ' feilet! ', e);
         return false;
     }
-};
-
-const useIaTjenesteMetrikkerEkstraDataRef = (): MutableRefObject<Partial<IaTjenesteMetrikkerEkstraData>> => {
-    const restvirksomhetsdata = useContext<RestVirksomhetsdata>(virksomhetsdataContext);
-    const dataFraEnhetsregisteret = useContext<EnhetsregisteretState>(enhetsregisteretContext);
-    const iaTjenesterMetrikkerEkstraData = useRef<Partial<IaTjenesteMetrikkerEkstraData>>({});
-
-    useEffect(() => {
-        if (
-            restvirksomhetsdata.status === RestStatus.Suksess &&
-            dataFraEnhetsregisteret.restUnderenhet.status === RestStatus.Suksess &&
-            dataFraEnhetsregisteret.restOverordnetEnhet.status === RestStatus.Suksess
-        ) {
-            iaTjenesterMetrikkerEkstraData.current = {
-                ...getIaTjenesteMetrikkerEkstraDataFravirksomhetsdata(restvirksomhetsdata),
-                ...getIaTjenesteMetrikkerEkstraDataFraEnhetsregisteret(
-                    dataFraEnhetsregisteret.restOverordnetEnhet,
-                    dataFraEnhetsregisteret.restUnderenhet,
-                    restvirksomhetsdata,
-                ),
-            };
-        }
-    }, [restvirksomhetsdata, dataFraEnhetsregisteret]);
-
-    return iaTjenesterMetrikkerEkstraData;
-};
-
-const byggIaTjenesteMetrikkerEkstraData = (
-    virksomhetsdata: Virksomhetsdata,
-    underenhet: Underenhet,
-    overordnetEnhet: OverordnetEnhet,
-): IaTjenesteMetrikkerEkstraData => {
-    const næringskode2siffer = virksomhetsdata.næringskode5Siffer.kode.substring(0, 2);
-    const næringsBeskrivelse = mapTilNæringsbeskrivelse(næringskode2siffer);
-    return {
-        antallAnsatte: virksomhetsdata.antallAnsatte,
-        næring2siffer: næringskode2siffer,
-        næring2SifferBeskrivelse:
-            næringsBeskrivelse === undefined ? 'INGEN_NÆRING' : næringsBeskrivelse,
-        næringKode5Siffer: virksomhetsdata.næringskode5Siffer.kode,
-        næringskode5SifferBeskrivelse: virksomhetsdata.næringskode5Siffer.beskrivelse,
-        bransje:
-            virksomhetsdata.bransje === undefined ? 'INGEN_BRANSJE' : virksomhetsdata.bransje,
-        orgnr: underenhet.orgnr,
-        institusjonellSektorKode: overordnetEnhet.institusjonellSektorkode,
-        fylkesnummer: 'IKKE_TILGJENGELIG',
-        fylke: 'IKKE_TILGJENGELIG',
-        kommunenummer: underenhet.beliggenhetsadresse.kommunenummer,
-        kommune: underenhet.beliggenhetsadresse.kommune,
-    };
-};
-
-const getIaTjenesteMetrikkerEkstraDataFravirksomhetsdata = (
-    restvirksomhetsdata: RestVirksomhetsdata,
-): Partial<IaTjenesteMetrikkerEkstraData> => {
-    if (restvirksomhetsdata.status === RestStatus.Suksess) {
-        const metrikker = restvirksomhetsdata.data;
-        const næringskode2siffer = metrikker.næringskode5Siffer.kode.substring(0, 2);
-
-        return {
-            antallAnsatte: metrikker.antallAnsatte,
-            næring2siffer: næringskode2siffer,
-            næring2SifferBeskrivelse: mapTilNæringsbeskrivelse(næringskode2siffer),
-            næringKode5Siffer: metrikker.næringskode5Siffer.kode,
-            næringskode5SifferBeskrivelse: metrikker.næringskode5Siffer.beskrivelse,
-            bransje: metrikker.bransje,
-        };
-    }
-    return {};
-};
-
-const getIaTjenesteMetrikkerEkstraDataFraEnhetsregisteret = (
-    restOverordnetEnhet: RestOverordnetEnhet,
-    restUnderenhet: RestUnderenhet,
-    restvirksomhetsdata: RestVirksomhetsdata,
-): Partial<IaTjenesteMetrikkerEkstraData> => {
-    if (
-        restvirksomhetsdata.status === RestStatus.Suksess &&
-        restOverordnetEnhet.status === RestStatus.Suksess &&
-        restUnderenhet.status === RestStatus.Suksess
-    ) {
-        return {
-            orgnr: restUnderenhet.data.orgnr,
-            institusjonellSektorKode: restOverordnetEnhet.data.institusjonellSektorkode,
-            fylkesnummer: 'IKKE_TILGJENGELIG',
-            fylke: 'IKKE_TILGJENGELIG',
-            kommunenummer: restUnderenhet.data.beliggenhetsadresse.kommunenummer,
-            kommune: restUnderenhet.data.beliggenhetsadresse.kommune,
-        };
-    }
-    return {};
 };
 
 export const useSendIaTjenesteMetrikkMottattVedSidevisningEvent = () => {
@@ -253,15 +94,14 @@ export const useSendIaTjenesteMetrikkMottattVedSidevisningEvent = () => {
 
     useEffect(() => {
         const iaTjenesteMetrikk = byggForenkletIaTjenesteMottattMetrikk(orgnr);
-        // TODO: Fjern timer
-        let timerFunc = setTimeout(() => {
-            const erIaTjenesterMetrikkerSendtForDenneBedrift =
-                erIaTjenesterMetrikkerSendtForBedrift(
-                    orgnr,
-                    context.bedrifterSomHarSendtMetrikker,
-                );
-            if (!erIaTjenesterMetrikkerSendtForDenneBedrift) {
-                sendForenkletIATjenesteMetrikk(iaTjenesteMetrikk).then((isSent) => {
+        const erIaTjenesterMetrikkerSendtForDenneBedrift =
+            erIaTjenesterMetrikkerSendtForBedrift(
+                orgnr,
+                context.bedrifterSomHarSendtMetrikker,
+            );
+        if (!erIaTjenesterMetrikkerSendtForDenneBedrift) {
+            sendForenkletIATjenesteMetrikk(iaTjenesteMetrikk)
+                .then((isSent) => {
                     if (isSent) {
                         context.setBedrifterSomHarSendtMetrikker(
                             iaTjenesterMetrikkerErSendtForBedrift(
@@ -271,8 +111,6 @@ export const useSendIaTjenesteMetrikkMottattVedSidevisningEvent = () => {
                         );
                     }
                 });
-            }
-        }, 5000);
-        return () => clearTimeout(timerFunc);
+        }
     }, [useOrgnr()]);
 };
