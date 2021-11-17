56b5d854d4e5db97ed9adb69a1d11f6521d551da
runarskagestad <runar.skagestad@nav.no>
2021-11-17 14:02:37 +0100

    Installerte testing-library, skrev komponenttest for ArbeidsmiljøportalenPanel

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
+import { ArbeidsmiljøportalPanel } from './Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel';
+import { RestStatus } from './api/api-utils';
+import { getvirksomhetsdataMock } from './mocking/virksomhetsdata-mock';
+import { ArbeidsmiljøportalenBransje } from './utils/bransje-utils';
+import { RestVirksomhetsdata } from './api/virksomhetsdata-api';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<App />);
+});
+
+
+it('ArbeidsmiljøportalenPanel rendrer med riktig link basert på bransje', () => {
+
+    const virksomhetMock: RestVirksomhetsdata = {
+        status: RestStatus.Suksess,
+        data: getvirksomhetsdataMock(ArbeidsmiljøportalenBransje.BARNEHAGER),
+    };
+    const { getByText } = render(<ArbeidsmiljøportalPanel restvirksomhetsdata={virksomhetMock} />);
+
+    const element = getByText('Gå til Arbeidsmiljøportalen') as HTMLAnchorElement;
+
+    expect(element.href).toBe('https://www.arbeidsmiljoportalen.no/bransje/barnehage');
+
 });
