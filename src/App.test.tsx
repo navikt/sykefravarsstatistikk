f6fd09f5521b000df6a53c105bd82d9d77a98516
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-11-18 12:30:51 +0100

    Eslint fixes again

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,13 +7,12 @@ import { getvirksomhetsdataMock } from './mocking/virksomhetsdata-mock';
 import { ArbeidsmiljøportalenBransje } from './utils/bransje-utils';
 import { RestVirksomhetsdata } from './api/virksomhetsdata-api';
 
+// eslint-disable-next-line jest/expect-expect
 it('renders without crashing', () => {
     render(<App />);
 });
 
-
 it('ArbeidsmiljøportalenPanel rendrer med riktig link basert på bransje', () => {
-
     const virksomhetMock: RestVirksomhetsdata = {
         status: RestStatus.Suksess,
         data: getvirksomhetsdataMock(ArbeidsmiljøportalenBransje.BARNEHAGER),
@@ -23,5 +22,4 @@ it('ArbeidsmiljøportalenPanel rendrer med riktig link basert på bransje', () =
     const element = getByText('Gå til Arbeidsmiljøportalen') as HTMLAnchorElement;
 
     expect(element.href).toBe('https://www.arbeidsmiljoportalen.no/bransje/barnehage');
-
 });
