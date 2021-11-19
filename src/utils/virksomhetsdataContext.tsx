7c5d8c4c928a4046997dac716dea1a8f644e854d
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-11-19 16:09:35 +0100

    Remove a provider

--- src/utils/virksomhetsdataContext.tsx
+++ src/utils/virksomhetsdataContext.tsx
@@ -1,15 +1,7 @@
-import React, { createContext, FunctionComponent } from 'react';
+import { createContext } from 'react';
 import { RestStatus } from '../api/api-utils';
 import { RestVirksomhetsdata } from '../api/virksomhetsdata-api';
-import { useVirksomhetsdata } from '../hooks/useVirksomhetsdata';
 
 export const virksomhetsdataContext = createContext<RestVirksomhetsdata>({
     status: RestStatus.IkkeLastet,
 });
-
-export const VirksomhetsdataProvider: FunctionComponent = (props) => {
-    const restVirksomhetsdata = useVirksomhetsdata();
-
-    const Provider = virksomhetsdataContext.Provider;
-    return <Provider value={restVirksomhetsdata}>{props.children}</Provider>;
-};
