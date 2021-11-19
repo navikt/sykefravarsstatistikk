7a73dc11bb36df6b80b9ece537f06a4023fa576d
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-11-19 16:03:43 +0100

    Add eslintignore and remove provider in enhetsregistercontext

--- src/utils/enhetsregisteretContext.tsx
+++ src/utils/enhetsregisteretContext.tsx
@@ -1,6 +1,6 @@
-import React, { createContext, FunctionComponent } from 'react';
+import { createContext } from 'react';
 import { RestStatus } from '../api/api-utils';
-import { EnhetsregisteretState, useEnheter } from '../hooks/useEnheter';
+import { EnhetsregisteretState } from '../hooks/useEnheter';
 
 export const enhetsregisteretContext = createContext<EnhetsregisteretState>({
     restUnderenhet: {
@@ -10,16 +10,3 @@ export const enhetsregisteretContext = createContext<EnhetsregisteretState>({
         status: RestStatus.IkkeLastet,
     },
 });
-
-export const EnhetsregisteretProvider: FunctionComponent = (props) => {
-    const { restOverordnetEnhet, restUnderenhet } = useEnheter();
-
-    const Provider = enhetsregisteretContext.Provider;
-
-    const contextValue: EnhetsregisteretState = {
-        restUnderenhet,
-        restOverordnetEnhet,
-    };
-
-    return <Provider value={contextValue}>{props.children}</Provider>;
-};
