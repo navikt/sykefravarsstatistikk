cb2ee29bbe3cb9f037e01b6dd781e4804a3e8cf6
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-11-23 14:18:29 +0100

    Render appcontent med mock data i app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from './App';
+import { AppContent } from './App';
 import { render } from '@testing-library/react';
 import { ArbeidsmiljøportalPanel } from './Forside/ArbeidsmiljøportalPanel/ArbeidsmiljøportalPanel';
 import { RestStatus } from './api/api-utils';
@@ -9,28 +9,13 @@ import { RestVirksomhetsdata } from './api/virksomhetsdata-api';
 import { BASE_PATH } from './konstanter';
 import { BrowserRouter } from 'react-router-dom';
 import { amplitudeMock } from './mocking/amplitude-mock';
+import { mockSykefraværNoEkstradata } from './mocking/data-mocks';
 
 // eslint-disable-next-line jest/expect-expect
 it('renders without crashing', () => {
-    // const meta: Sykefravarsstatistikk = {
-    //     virksomhetsdata: {
-    //         status: RestStatus.Suksess,
-    //         data: {
-    //             antallAnsatte: 2,
-    //             bransje: ArbeidsmiljøportalenBransje.ANDRE_BRANSJER,
-    //             næringskode5Siffer: {
-    //                 beskrivelse: 'hei',
-    //                 kode: 'på deg',
-    //             },
-    //         },
-    //     },
-    // };
-    //
-    // render(<AppContent />);
-
     render(
         <BrowserRouter basename={BASE_PATH}>
-            <App analyticsClient={amplitudeMock} />
+            <AppContent analyticsClient={amplitudeMock} {...mockSykefraværNoEkstradata} />
         </BrowserRouter>
     );
 });
