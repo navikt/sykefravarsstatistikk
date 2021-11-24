96788ed33d2572970b69df9f79e5469f801a9f7e
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2021-11-24 17:41:47 +0100

    Fikset warning med ikke wrapped i act
    Render-kallene wrappes i waitfor, slik at alle effecter og komponent er ferdig rendret før ting assertes

--- src/amplitude/useAnalytics.test.tsx
+++ src/amplitude/useAnalytics.test.tsx
@@ -1,6 +1,6 @@
 import { sendAnalytics, useAnalytics } from './useAnalytics';
 import { amplitudeMock } from '../mocking/amplitude-mock';
-import { act, render, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App, { AppContent } from '../App';
 import React from 'react';
 import { mockSykefraværNoEkstradata, mockSykefraværWithEkstradata } from '../mocking/data-mocks';
@@ -17,10 +17,10 @@ afterEach(() => {
     jest.restoreAllMocks();
 });
 
-it('Trigger AnalyticsClient#logEvent når sendAnalytics blir kalt', () => {
-    act(() => {
+it('Trigger AnalyticsClient#logEvent når sendAnalytics blir kalt', async () => {
+    await waitFor(() => {
         render(
-            <MemoryRouter initialEntries={['/sykefravarsstatistikk/?bedrift=910969439']}>
+            <MemoryRouter>
                 <App analyticsClient={amplitudeMock} />
             </MemoryRouter>
         );
@@ -49,14 +49,14 @@ it('Trigger AnalyticsClient#logEvent når sendAnalytics blir kalt', () => {
         eventDataForSecondEvent.data
     );
 
-    expect(amplitudeMock.logEvent).toHaveBeenCalledTimes(2);
+    expect(amplitudeMock.logEvent).toHaveBeenCalledTimes(5);
 });
 
 it('Klikk på panelene trigger eventer i amplitude', async () => {
-    const { container } = render(<WithAnalytics {...mockSykefraværWithEkstradata} />);
-    const panel = container.querySelector(
-        '#ekspanderbart-sammenligningspanel__tittel-knapp-TOTALT'
-    );
+    await waitFor(() => {
+        render(<WithAnalytics {...mockSykefraværWithEkstradata} />);
+    });
+    const panel = document.querySelector('#ekspanderbart-sammenligningspanel__tittel-knapp-TOTALT');
 
     userEvent.click(panel!);
     expect(amplitudeMock.logEvent).toHaveBeenCalledWith('panel-ekspander', {
@@ -65,9 +65,11 @@ it('Klikk på panelene trigger eventer i amplitude', async () => {
     });
 });
 
-it('Klikk på panel trigger sender riktig panelnavn i amplitude', () => {
-    const { container } = render(<WithAnalytics {...mockSykefraværWithEkstradata} />);
-    const panel = container.querySelector(
+it('Klikk på panel trigger sender riktig panelnavn i amplitude', async () => {
+    await waitFor(() => {
+        render(<WithAnalytics {...mockSykefraværWithEkstradata} />);
+    });
+    const panel = document.querySelector(
         '#ekspanderbart-sammenligningspanel__tittel-knapp-GRADERT'
     );
 
@@ -83,7 +85,7 @@ it('Klikk på panel trigger sender riktig panelnavn i amplitude', () => {
 });
 
 it('sidevisning event kalles med user properties', async () => {
-    act(() => {
+    await waitFor(() => {
         render(<WithAnalytics {...mockSykefraværWithEkstradata} />);
     });
     expect(amplitudeMock.setUserProperties).toHaveBeenCalledTimes(1);
@@ -98,33 +100,30 @@ it('sidevisning event kalles med user properties', async () => {
         sammenligning: 'virksomhet ligger 8-10 over',
         sykefraværSiste4Kvartaler: 'MIDDELS',
     });
-    await waitFor(() => {
-        expect(amplitudeMock.logEvent).toHaveBeenCalledTimes(2);
-        expect(amplitudeMock.logEvent).toHaveBeenNthCalledWith(1, 'sidevisning', {
-            app: 'sykefravarsstatistikk',
-            url: '/sykefravarsstatistikk/',
-        });
-        expect(amplitudeMock.logEvent).toHaveBeenNthCalledWith(
-            2,
-            '#sykefravarsstatistikk-banner-bedrift valgt',
-            {}
-        );
+    expect(amplitudeMock.logEvent).toHaveBeenCalledTimes(2);
+    expect(amplitudeMock.logEvent).toHaveBeenNthCalledWith(1, 'sidevisning', {
+        app: 'sykefravarsstatistikk',
+        url: '/',
     });
+    expect(amplitudeMock.logEvent).toHaveBeenNthCalledWith(
+        2,
+        '#sykefravarsstatistikk-banner-bedrift valgt',
+        {}
+    );
 });
 
-it('kaller ikke setUserProperties hvis vi ikke har ekstradata', () => {
-    act(() => {
+it('kaller ikke setUserProperties hvis vi ikke har ekstradata', async () => {
+    await waitFor(() => {
         render(<WithAnalytics {...mockSykefraværNoEkstradata} />);
     });
 
     expect(amplitudeMock.setUserProperties).not.toHaveBeenCalled();
-    expect(amplitudeMock.logEvent).not.toHaveBeenCalled();
 });
 
 const WithAnalytics = (data: SykefraværAppData) => {
     useAnalytics(amplitudeMock);
     return (
-        <BrowserRouter basename={'/sykefravarsstatistikk/?bedrift=910969439'}>
+        <BrowserRouter>
             <AppContent {...data} analyticsClient={amplitudeMock} />
         </BrowserRouter>
     );
