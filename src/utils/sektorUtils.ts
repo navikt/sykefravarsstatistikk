6c16de9919aacf67eaf934185799098af762f5a8
runarskagestad <runar.skagestad@nav.no>
2021-12-07 08:47:18 +0100

    fikset skrivefeil i mapTilPrivatEllerOffentligSektor

--- src/utils/sektorUtils.ts
+++ src/utils/sektorUtils.ts
@@ -3,7 +3,7 @@ import { InstitusjonellSektorkode } from '../api/enhetsregisteret-api';
 export type Sektor = 'offentlig' | 'privat';
 
 export const sektorkoderForOffentligeVirksomheter = ['6100', '6500', '3900', '3100'];
-export const mapTilPrivatElleOffentligSektor = (
+export const mapTilPrivatEllerOffentligSektor = (
     institusjonellSektorkode: InstitusjonellSektorkode
 ): Sektor => {
     return sektorkoderForOffentligeVirksomheter.includes(institusjonellSektorkode.kode)
