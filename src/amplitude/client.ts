91d7a1dff0ca216eb1d5fe866976a99223d46f26
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-01-26 16:30:47 +0100

    Bytte til Amplitude-endepunkt '/collect-auto' ifm overgang til EU-servere

--- src/amplitude/client.ts
+++ src/amplitude/client.ts
@@ -2,18 +2,14 @@ import amplitude from 'amplitude-js';
 import { EventProperties } from './events';
 
 function initializeAmplitudeClient(): AnalyticsClient {
-    const bucketId =
-        window.location.hostname === 'arbeidsgiver.nav.no'
-            ? '3a6fe32c3457e77ce81c356bb14ca886'
-            : '55477baea93c5227d8c0f6b813653615';
-
     const amplitudeInstance = amplitude.getInstance();
-    amplitudeInstance.init(bucketId, '', {
-        apiEndpoint: 'amplitude.nav.no/collect',
+
+    amplitudeInstance.init('default', undefined, {
+        apiEndpoint: 'amplitude.nav.no/collect-auto',
         saveEvents: false,
         includeUtm: true,
-        batchEvents: false,
         includeReferrer: true,
+        platform: window.location.toString(),
     });
 
     return amplitudeInstance;
