ae111f8d3ee7c36f0a364b94535951c401d4e09f
runarskagestad <runar.skagestad@nav.no>
2022-05-30 10:29:35 +0200

    Navngav Amplitude-buckets for økt lesbarhet

--- src/amplitude/client.ts
+++ src/amplitude/client.ts
@@ -1,11 +1,16 @@
 import amplitude from 'amplitude-js';
 import { EventProperties } from './events';
 
+const AmplitudeBucket = {
+    ARBEIDSGIVER_PROD: 'a8243d37808422b4c768d31c88a22ef4',
+    ARBEIDSGIVER_DEV: '6ed1f00aabc6ced4fd6fcb7fcdc01b30',
+};
+
 function initializeAmplitudeClient(): AnalyticsClient {
     const bucketId =
         window.location.hostname === 'arbeidsgiver.nav.no'
-            ? 'a8243d37808422b4c768d31c88a22ef4'
-            : '6ed1f00aabc6ced4fd6fcb7fcdc01b30';
+            ? AmplitudeBucket.ARBEIDSGIVER_PROD
+            : AmplitudeBucket.ARBEIDSGIVER_DEV;
 
     const amplitudeInstance = amplitude.getInstance();
     amplitudeInstance.init(bucketId, '', {
