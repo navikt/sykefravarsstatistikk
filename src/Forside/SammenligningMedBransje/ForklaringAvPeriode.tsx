42759ed08053b00d53cd3701039a972e3fc0d49b
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2022-10-07 10:06:41 +0200

    skriv om react-komponenter til bedre practice, fjerne gamle kommentarer

--- src/Forside/SammenligningMedBransje/ForklaringAvPeriode.tsx
+++ src/Forside/SammenligningMedBransje/ForklaringAvPeriode.tsx
@@ -1,95 +1,51 @@
-import React, {FunctionComponent, ReactElement} from 'react';
-import {Ingress, Normaltekst} from 'nav-frontend-typografi';
-import {
-  formatterDatoMedMånedNavn,
-  getPeriodeMedDato,
-} from '../../utils/app-utils';
+import React, { FunctionComponent, ReactElement } from 'react';
+import { Ingress } from 'nav-frontend-typografi';
 import './ForklaringAvPeriode.less';
-import {SammenligningsType} from '../vurderingstekster';
+import { SammenligningsType } from '../vurderingstekster';
 import classNames from 'classnames';
-import {
-  RestPubliseringsdatoer
-} from "../../api/publiseringsdatoer-api";
-import {RestStatus} from "../../api/api-utils";
-import NavFrontendSpinner from "nav-frontend-spinner";
+import { RestPubliseringsdatoer } from '../../api/publiseringsdatoer-api';
+import { PeriodeForStatistikk } from './PeriodeForStatistikk';
+import { PubliseringsdatoOppdateringsinfo } from './PubliseringsdatoOppdateringsinfo';
 
 interface Props {
-  sammenligningsType: SammenligningsType;
-  className?: string;
-  restPubliseringsdatoer: RestPubliseringsdatoer;
+    sammenligningsType: SammenligningsType;
+    className?: string;
+    restPubliseringsdatoer: RestPubliseringsdatoer;
 }
 
-export const ForklaringAvPeriode: FunctionComponent<Props> = (
-    {
-      sammenligningsType,
-      className,
-      restPubliseringsdatoer,
-    }) => {
+export const ForklaringAvPeriode: FunctionComponent<Props> = ({
+    sammenligningsType,
+    className,
+    restPubliseringsdatoer,
+}) => {
+    const getTittel = (sammenligningsType: SammenligningsType): ReactElement => {
+        switch (sammenligningsType) {
+            case SammenligningsType.TOTALT:
+                return <>Legemeldt sykefravær</>;
+            case SammenligningsType.KORTTID:
+                return (
+                    <>
+                        Andel legemeldt korttidsfravær <br /> fra 1. til 16. dag
+                    </>
+                );
+            case SammenligningsType.LANGTID:
+                return (
+                    <>
+                        Andel legemeldt langtidsfravær <br /> fra 17. dag
+                    </>
+                );
+            case SammenligningsType.GRADERT:
+                return <>Gradert sykmelding</>;
+        }
+    };
 
-  const getPubliseringsdatoinfoElement = (): ReactElement => {
-    if (restPubliseringsdatoer.status === RestStatus.Suksess) {
-      const publiseringsdatoer = restPubliseringsdatoer.data;
-      return (
-          <div>
-            <Normaltekst>{`Sist oppdatert: `+ formatterDatoMedMånedNavn(new Date(publiseringsdatoer.sistePubliseringsdato))}</Normaltekst>
-            <Normaltekst>{`Neste oppdatering: `+ formatterDatoMedMånedNavn(new Date(publiseringsdatoer.nestePubliseringsdato))}</Normaltekst>
-          </div>
-      )
-    } else if (
-        restPubliseringsdatoer.status === RestStatus.LasterInn ||
-        restPubliseringsdatoer.status === RestStatus.IkkeLastet
-    ) {
-      return <NavFrontendSpinner/>
-    } else {
-      return <Normaltekst>{""}</Normaltekst>
-    }
-  }
-
-  const getPeriodeElement = (): ReactElement => {
-    if (restPubliseringsdatoer.status === RestStatus.Suksess) {
-      return (
-          <div>
-            <Normaltekst>{`Periode: `+ getPeriodeMedDato(restPubliseringsdatoer.data.gjeldendePeriode)}</Normaltekst>
-          </div>
-      )
-    } else if (
-        restPubliseringsdatoer.status === RestStatus.IkkeLastet ||
-        restPubliseringsdatoer.status === RestStatus.LasterInn
-    ) {
-      return <NavFrontendSpinner/>
-    } else {
-      return <Normaltekst>{""}</Normaltekst>
-    }
-  }
-
-  const getTittel = (sammenligningsType: SammenligningsType): ReactElement => {
-    switch (sammenligningsType) {
-      case SammenligningsType.TOTALT:
-        return <>Legemeldt sykefravær</>;
-      case SammenligningsType.KORTTID:
-        return (
-            <>
-              Andel legemeldt korttidsfravær <br/> fra 1. til 16. dag
-            </>
-        );
-      case SammenligningsType.LANGTID:
-        return (
-            <>
-              Andel legemeldt langtidsfravær <br/> fra 17. dag
-            </>
-        );
-      case SammenligningsType.GRADERT:
-        return <>Gradert sykmelding</>;
-    }
-  };
-
-  return (
-      <div className={classNames('forklaring-av-periode', className)}>
-        <Ingress tag='h3' className='forklaring-av-periode__tittel'>
-          <strong>{getTittel(sammenligningsType)}</strong>
-        </Ingress>
-        {getPeriodeElement()}
-        {getPubliseringsdatoinfoElement()}
-      </div>
-  );
+    return (
+        <div className={classNames('forklaring-av-periode', className)}>
+            <Ingress tag="h3" className="forklaring-av-periode__tittel">
+                <strong>{getTittel(sammenligningsType)}</strong>
+            </Ingress>
+            <PeriodeForStatistikk restPubliseringsdatoer={restPubliseringsdatoer} />
+            <PubliseringsdatoOppdateringsinfo restPubliseringsdatoer={restPubliseringsdatoer} />
+        </div>
+    );
 };
--- src/Forside/SlikHarViKommetFramTilDittResultat/SlikHarViKommetFramTilDittResultatTekst.tsx
+++ src/Forside/SlikHarViKommetFramTilDittResultat/SlikHarViKommetFramTilDittResultatTekst.tsx
@@ -1,132 +1,111 @@
-import React, {FunctionComponent, ReactElement} from "react";
-import {SykefraværVurdering} from "../Speedometer/Speedometer";
-import {ÅrstallOgKvartal} from "../../utils/sykefraværshistorikk-utils";
-import {Normaltekst} from "nav-frontend-typografi";
-import "./SlikHarViKommetFramTilDittResultatTekst.less";
-import {LenkeTilHistorikk} from "../../felleskomponenter/LenkeTilHistorikk";
-import {getPeriodeMedDato} from "../../utils/app-utils";
-import {RestPubliseringsdatoer} from "../../api/publiseringsdatoer-api";
-import {RestStatus} from "../../api/api-utils";
-import NavFrontendSpinner from "nav-frontend-spinner";
+import React, { FunctionComponent } from 'react';
+import { SykefraværVurdering } from '../Speedometer/Speedometer';
+import { ÅrstallOgKvartal } from '../../utils/sykefraværshistorikk-utils';
+import { Normaltekst } from 'nav-frontend-typografi';
+import './SlikHarViKommetFramTilDittResultatTekst.less';
+import { LenkeTilHistorikk } from '../../felleskomponenter/LenkeTilHistorikk';
+import { RestPubliseringsdatoer } from '../../api/publiseringsdatoer-api';
+import { PeriodeForBeskrivelse } from './PeriodeForBeskrivelse';
 
 interface Props {
-  resultat: SykefraværVurdering;
-  kvartaler?: ÅrstallOgKvartal[];
-  restPubliseringsdatoer: RestPubliseringsdatoer;
+    resultat: SykefraværVurdering;
+    kvartaler?: ÅrstallOgKvartal[];
+    restPubliseringsdatoer: RestPubliseringsdatoer;
 }
 
 export const SlikHarViKommetFramTilDittResultatTekst: FunctionComponent<Props> = ({
-                                                                                    resultat,
-                                                                                    kvartaler,
-                                                                                    restPubliseringsdatoer,
-                                                                                  }) => {
-  const getPeriodeElement = (): ReactElement => {
-    if (restPubliseringsdatoer.status === RestStatus.Suksess) {
-      return (
-          <Normaltekst>{
-              `Tallene er beregnet på sykefraværsstatistikk fra `+
-              getPeriodeMedDato(restPubliseringsdatoer.data.gjeldendePeriode) +
-              ` (fire siste kvartaler).`
-          }</Normaltekst>
-      )
-    } else if (
-        restPubliseringsdatoer.status === RestStatus.LasterInn ||
-        restPubliseringsdatoer.status === RestStatus.IkkeLastet
-    ) {
-      return <NavFrontendSpinner/>
-    } else {
-      return <Normaltekst>{""}</Normaltekst>
+    resultat,
+    kvartaler,
+    restPubliseringsdatoer,
+}) => {
+    switch (resultat) {
+        case SykefraværVurdering.OVER:
+        case SykefraværVurdering.MIDDELS:
+        case SykefraværVurdering.UNDER:
+            return (
+                <>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
+                        som NAV og Statistisk sentralbyrå (SSB) utarbeider.
+                    </Normaltekst>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Sammenligningen tar ikke ta hensyn til din virksomhetens størrelse.
+                    </Normaltekst>
+                    <Normaltekst>Tallene er beregnet på sykefraværsstatistikk fra:</Normaltekst>
+                    <Kvartalsliste kvartaler={kvartaler} />
+                    <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total" />
+                </>
+            );
+        case SykefraværVurdering.MASKERT:
+            return (
+                <>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
+                        som NAV og Statistisk sentralbyrå (SSB) utarbeider.
+                    </Normaltekst>
+                    <Normaltekst>
+                        Sammenligningen tar ikke ta hensyn til din virksomhetens størrelse.
+                    </Normaltekst>
+                    <Normaltekst>
+                        Bransjens tall er beregnet på sykefraværsstatistikk fra:
+                    </Normaltekst>
+                    <PeriodeForBeskrivelse restPubliseringsdatoer={restPubliseringsdatoer} />
+                    <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total" />
+                </>
+            );
+        case SykefraværVurdering.UFULLSTENDIG_DATA:
+            return (
+                <>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
+                        som NAV og Statistisk sentralbyrå (SSB) utarbeider.
+                    </Normaltekst>
+                    <Normaltekst>Dine tall er beregnet på sykefraværsstatistikk fra:</Normaltekst>
+                    <Kvartalsliste kvartaler={kvartaler} />
+                    <Normaltekst>
+                        Bransjens tall er beregnet på sykefraværsstatistikk fra:
+                    </Normaltekst>
+                    <PeriodeForBeskrivelse restPubliseringsdatoer={restPubliseringsdatoer} />
+                    <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total" />
+                </>
+            );
+        case SykefraværVurdering.INGEN_DATA:
+            return (
+                <>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
+                        som NAV og Statistisk sentralbyrå (SSB) utarbeider.
+                    </Normaltekst>
+                    <Normaltekst>
+                        Bransjens tall er beregnet på sykefraværsstatistikk fra:
+                    </Normaltekst>
+                    <PeriodeForBeskrivelse restPubliseringsdatoer={restPubliseringsdatoer} />
+                </>
+            );
+        case SykefraværVurdering.FEIL:
+            return (
+                <>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
+                        som NAV og Statistisk sentralbyrå (SSB) utarbeider.
+                    </Normaltekst>
+                    <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
+                        Tallene er beregnet på sykefraværsstatistikk fra:
+                    </Normaltekst>
+                    <Normaltekst>—</Normaltekst>
+                </>
+            );
+        default:
+            return null;
     }
-  }
-
-  switch (resultat) {
-    case SykefraværVurdering.OVER:
-    case SykefraværVurdering.MIDDELS:
-    case SykefraværVurdering.UNDER:
-      return (
-          <>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
-              som NAV og Statistisk sentralbyrå (SSB) utarbeider.
-            </Normaltekst>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Sammenligningen tar ikke ta hensyn til din virksomhetens størrelse.
-            </Normaltekst>
-            <Normaltekst>Tallene er beregnet på sykefraværsstatistikk fra:</Normaltekst>
-            <Kvartalsliste kvartaler={kvartaler}/>
-            <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total"/>
-          </>
-      );
-    case SykefraværVurdering.MASKERT:
-      return (
-          <>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
-              som NAV og Statistisk sentralbyrå (SSB) utarbeider.
-            </Normaltekst>
-            <Normaltekst>
-              Sammenligningen tar ikke ta hensyn til din virksomhetens størrelse.
-            </Normaltekst>
-            <Normaltekst>
-              Bransjens tall er beregnet på sykefraværsstatistikk fra:
-            </Normaltekst>
-            {getPeriodeElement()}
-            <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total"/>
-          </>
-      );
-    case SykefraværVurdering.UFULLSTENDIG_DATA:
-      return (
-          <>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
-              som NAV og Statistisk sentralbyrå (SSB) utarbeider.
-            </Normaltekst>
-            <Normaltekst>Dine tall er beregnet på sykefraværsstatistikk fra:</Normaltekst>
-            <Kvartalsliste kvartaler={kvartaler}/>
-            <Normaltekst>
-              Bransjens tall er beregnet på sykefraværsstatistikk fra:
-            </Normaltekst>
-            {getPeriodeElement()}
-            <LenkeTilHistorikk kildeSomSendesMedEvent="les mer total"/>
-          </>
-      );
-    case SykefraværVurdering.INGEN_DATA:
-      return (
-          <>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
-              som NAV og Statistisk sentralbyrå (SSB) utarbeider.
-            </Normaltekst>
-            <Normaltekst>
-              Bransjens tall er beregnet på sykefraværsstatistikk fra:
-            </Normaltekst>
-            {getPeriodeElement()}
-          </>
-      );
-    case SykefraværVurdering.FEIL:
-      return (
-          <>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Legemeldt sykefravær i sammenligningen er hentet fra sykefraværsstatistikken
-              som NAV og Statistisk sentralbyrå (SSB) utarbeider.
-            </Normaltekst>
-            <Normaltekst className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__paragraf">
-              Tallene er beregnet på sykefraværsstatistikk fra:
-            </Normaltekst>
-            <Normaltekst>—</Normaltekst>
-          </>
-      );
-    default:
-      return null;
-  }
 };
 
-const Kvartalsliste: FunctionComponent<{ kvartaler?: ÅrstallOgKvartal[] }> = ({kvartaler}) => (
+const Kvartalsliste: FunctionComponent<{ kvartaler?: ÅrstallOgKvartal[] }> = ({ kvartaler }) => (
     <ul className="slik-har-vi-kommet-fram-til-ditt-resultat-tekst__kvartalsliste">
-      {kvartaler?.map((kvartal, index) => (
-          <Normaltekst tag="li" key={index}>
-            {kvartal.kvartal}. kvartal {kvartal.årstall}
-          </Normaltekst>
-      ))}
+        {kvartaler?.map((kvartal, index) => (
+            <Normaltekst tag="li" key={index}>
+                {kvartal.kvartal}. kvartal {kvartal.årstall}
+            </Normaltekst>
+        ))}
     </ul>
 );
