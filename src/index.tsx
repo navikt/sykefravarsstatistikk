f58ff65c17a600b19ef27f00a3a591ef2c49f60a
runarskagestad <runar.skagestad@nav.no>
2022-12-15 14:21:17 +0100

    Injecting decorator client side, fixing issues on local run

--- src/index.tsx
+++ src/index.tsx
@@ -4,29 +4,37 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as Sentry from '@sentry/browser';
 import './index.less';
-import {getMiljø} from './utils/miljøUtils';
-import {BASE_PATH} from './konstanter';
-import {BrowserRouter} from 'react-router-dom';
-import {amplitudeClient} from './amplitude/client';
+import { getMiljø } from './utils/miljøUtils';
+import { BASE_PATH, MILJØ } from './konstanter';
+import { BrowserRouter } from 'react-router-dom';
+import { amplitudeClient } from './amplitude/client';
+import { injectDecoratorClientSide } from '@navikt/nav-dekoratoren-moduler';
 import SamtalestøttePodletpanel from './Forside/Samtalestøttepanel/SamtalestøttePodletpanel';
 
 if (process.env.REACT_APP_MOCK || getMiljø() === 'labs-gcp') {
-  console.log('========================================');
-  console.log('=============== MED MOCK ===============');
-  console.log('===DETTE SKAL DU IKKE SE I PRODUKSJON===');
-  console.log('========================================');
-  require('./mocking/mock');
+    console.log('========================================');
+    console.log('=============== MED MOCK ===============');
+    console.log('===DETTE SKAL DU IKKE SE I PRODUKSJON===');
+    console.log('========================================');
+    require('./mocking/mock');
 }
 
 Sentry.init({
-  dsn: 'https://c4ef091d1fb54f01a7f808e621b28948@sentry.gc.nav.no/13',
-  environment: getMiljø(),
-  enabled: getMiljø() !== 'local',
+    dsn: 'https://c4ef091d1fb54f01a7f808e621b28948@sentry.gc.nav.no/13',
+    environment: getMiljø(),
+    enabled: getMiljø() !== 'local',
 });
+
+injectDecoratorClientSide({
+    env: getMiljø() === MILJØ.PROD ? 'prod' : 'dev',
+    context: 'arbeidsgiver',
+    redirectToApp: true,
+    level: 'Level4',
+}).catch(Sentry.captureException);
 
 ReactDOM.render(
     <BrowserRouter basename={BASE_PATH}>
-      <App analyticsClient={amplitudeClient} samtalestøttePodlet={<SamtalestøttePodletpanel/>}/>
+        <App analyticsClient={amplitudeClient} samtalestøttePodlet={<SamtalestøttePodletpanel />} />
     </BrowserRouter>,
     document.getElementById('root')
 );
