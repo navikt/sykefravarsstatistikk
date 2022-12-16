2ce8921c98ab639cb24094c7f77478084dc123b4
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2022-12-16 13:44:47 +0100

    fjern gammel dekoratør med mustache

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const path = require('path');
 const express = require('express');
-const getDecorator = require('./decorator');
-const mustacheExpress = require('mustache-express');
 const proxy = require('./proxy');
-const iaTjenesterMetrikkerProxy = require("./iaTjenesterMetrikkerProxy");
-const {BASE_PATH} = require('./konstanter');
+const iaTjenesterMetrikkerProxy = require('./iaTjenesterMetrikkerProxy');
+const { BASE_PATH } = require('./konstanter');
 const buildPath = path.join(__dirname, '../build');
 const dotenv = require('dotenv');
-const {initIdporten} = require('./idporten');
-const {initTokenX} = require('./tokenx');
+const { initIdporten } = require('./idporten');
+const { initTokenX } = require('./tokenx');
 const cookieParser = require('cookie-parser');
 const getCspValue = require('./content-security-policy');
 
@@ -18,94 +16,73 @@ const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 app.use(cookieParser());
-app.engine('html', mustacheExpress());
-app.set('view engine', 'mustache');
 app.set('views', buildPath);
 
-const renderAppMedDecorator = (decoratorFragments) => {
-  return new Promise((resolve, reject) => {
-    app.render('index.html', decoratorFragments, (err, html) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(html);
-      }
-    });
-  });
-};
-
 const startServer = async (html) => {
-  console.log('Starting server: server.js');
+    console.log('Starting server: server.js');
 
-  await Promise.all([initIdporten(), initTokenX()]);
+    await Promise.all([initIdporten(), initTokenX()]);
 
-  app.disable('x-powered-by');
-  app.use((req, res, next) => {
-    res.header('X-Frame-Options', 'SAMEORIGIN');
-    res.header('X-Xss-Protection', '1; mode=block');
-    res.header('X-Content-Type-Options', 'nosniff');
-    res.header('Referrer-Policy', 'no-referrer');
-    res.header('Permissions-Policy',
-        'geolocation=(), microphone=(), camera=()');
-    res.header('Content-Security-Policy', getCspValue());
-    res.header('X-WebKit-CSP', getCspValue());
-    res.header('X-Content-Security-Policy', getCspValue());
+    app.disable('x-powered-by');
+    app.use((req, res, next) => {
+        res.header('X-Frame-Options', 'SAMEORIGIN');
+        res.header('X-Xss-Protection', '1; mode=block');
+        res.header('X-Content-Type-Options', 'nosniff');
+        res.header('Referrer-Policy', 'no-referrer');
+        res.header('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+        res.header('Content-Security-Policy', getCspValue());
+        res.header('X-WebKit-CSP', getCspValue());
+        res.header('X-Content-Security-Policy', getCspValue());
 
-    if (process.env.NODE_ENV === 'development') {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.header(
-          'Access-Control-Allow-Headers',
-          'Origin, X-Requested-With, Content-Type, Accept'
-      );
-      res.header('Access-Control-Allow-Methods', 'GET, POST');
-    }
-    next();
-  });
+        if (process.env.NODE_ENV === 'development') {
+            res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
+            res.header('Access-Control-Allow-Methods', 'GET, POST');
+        }
+        next();
+    });
 
-  app.use(BASE_PATH + '/', express.static(buildPath, {index: false}));
+    app.use(BASE_PATH + '/', express.static(buildPath, { index: false }));
 
-  app.get(`${BASE_PATH}/redirect-til-login`, (req, res) => {
-    const referrerUrl = `${process.env.APP_INGRESS}/success?redirect=${req.query.redirect}`;
-    res.redirect(BASE_PATH + `/oauth2/login?redirect=${referrerUrl}`);
-  });
+    app.get(`${BASE_PATH}/redirect-til-login`, (req, res) => {
+        const referrerUrl = `${process.env.APP_INGRESS}/success?redirect=${req.query.redirect}`;
+        res.redirect(BASE_PATH + `/oauth2/login?redirect=${referrerUrl}`);
+    });
 
-  app.get(`${BASE_PATH}/success`, (req, res) => {
-    const loginserviceToken = req.cookies['selvbetjening-idtoken'];
-    if (loginserviceToken && req.query.redirect.startsWith(
-        process.env.APP_INGRESS)) {
-      res.redirect(req.query.redirect);
-    } else if (req.query.redirect.startsWith(process.env.APP_INGRESS)) {
-      res.redirect(`${process.env.LOGIN_URL}${req.query.redirect}`);
-    } else {
-      res.redirect(`${process.env.LOGIN_URL}${process.env.APP_INGRESS}`);
-    }
-  });
+    app.get(`${BASE_PATH}/success`, (req, res) => {
+        const loginserviceToken = req.cookies['selvbetjening-idtoken'];
+        if (loginserviceToken && req.query.redirect.startsWith(process.env.APP_INGRESS)) {
+            res.redirect(req.query.redirect);
+        } else if (req.query.redirect.startsWith(process.env.APP_INGRESS)) {
+            res.redirect(`${process.env.LOGIN_URL}${req.query.redirect}`);
+        } else {
+            res.redirect(`${process.env.LOGIN_URL}${process.env.APP_INGRESS}`);
+        }
+    });
 
-  app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
-  app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
+    app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
+    app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
 
-  app.use(proxy);
-  app.use(iaTjenesterMetrikkerProxy);
+    app.use(proxy);
+    app.use(iaTjenesterMetrikkerProxy);
 
-  app.get(BASE_PATH, (req, res) => {
-    res.send(html);
-  });
+    app.get(BASE_PATH, (req, res) => {
+        res.send(html);
+    });
 
-  app.get(BASE_PATH + '/*', (req, res) => {
-    res.send(html);
-  });
+    app.get(BASE_PATH + '/*', (req, res) => {
+        res.send(html);
+    });
 
-  app.listen(PORT, () => {
-    console.log('Server listening on port', PORT);
-  });
+    app.listen(PORT, () => {
+        console.log('Server listening on port', PORT);
+    });
 };
 
-getDecorator()
-.then(renderAppMedDecorator, (error) => {
-  console.error('Kunne ikke hente dekoratør ', error);
-  process.exit(1);
-})
-.then(startServer, (error) => {
-  console.error('Kunne ikke rendre app ', error);
-  process.exit(1);
+app.render('index.html').then(startServer, (error) => {
+    console.error('Kunne ikke rendre app ', error);
+    process.exit(1);
 });
