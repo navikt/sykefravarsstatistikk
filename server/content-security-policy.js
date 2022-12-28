34df41d8d915de6560ea61f4a3899a3846f3f752
runarskagestad <runar.skagestad@nav.no>
2022-12-28 16:35:53 +0100

    adding manifest-scr to csp

--- server/content-security-policy.js
+++ server/content-security-policy.js
@@ -1,64 +1,64 @@
 const contentSecurityPolicy = {
-  'default-src': ["'self'", 'arbeidsgiver.nav.no', 'nav.no'],
-  'script-src': [
-    "'self'",
-    "'report-sample'",
-    "'unsafe-inline'",
-    "'unsafe-eval'",
-    'www.googletagmanager.com',
-    'static.hotjar.com',
-    'script.hotjar.com',
-    '*.psplugin.com',
-    '*.nav.no',
-  ],
-  'style-src': [
-    "'self'",
-    'blob:',
-    '*.nav.no',
-    "'unsafe-inline'",
-    'fonts.googleapis.com',
-    'translate.googleapis.com',
-  ],
-  'connect-src': [
-    "'self'",
-    '*.nav.no',
-    'oidc-ver2.difi.no/idporten-oidc-provider/authorize',
-    'idporten-ver2.difi.no/opensso/SSORedirect/metaAlias/norge.no/idp4',
-    'amplitude.nav.no',
-    'data.brreg.no',
-    '*.psplugin.com',
-    '*.hotjar.com',
-    '*.vc.hotjar.com',
-    '*.hotjar.io',
-    '*.vc.hotjar.io',
-    'wss://*.hotjar.com',
-    'api.puzzel.com',
-    'nav.boost.ai',
-    'sentry.gc.nav.no',
-    'ta-survey-v2.herokuapp.com',
-  ],
-  'font-src': ['data:', '*.psplugin.com', '*.hotjar.com', 'fonts.gstatic.com',
-    'www.nav.no'],
-  'frame-src': ['vars.hotjar.com', '*.nav.no'],
-  'img-src': [
-    "'self'",
-    '*.hotjar.com',
-    'www.gstatic.com',
-    'translate.google.com',
-    '*.nav.no',
-    'data:',
-    'www.googletagmanager.com',
-  ],
-  'report-uri': [
-    'https://sentry.gc.nav.no/api/13/security/?sentry_key=c4ef091d1fb54f01a7f808e621b28948',
-  ],
-  'worker-src': ["'self'", 'blob:'],
+    'default-src': ["'self'", 'arbeidsgiver.nav.no'],
+    'manifest-src': ['*.nav.no'],
+    'script-src': [
+        "'self'",
+        "'report-sample'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+        'www.googletagmanager.com',
+        'static.hotjar.com',
+        'script.hotjar.com',
+        '*.psplugin.com',
+        '*.nav.no',
+    ],
+    'style-src': [
+        "'self'",
+        'blob:',
+        '*.nav.no',
+        "'unsafe-inline'",
+        'fonts.googleapis.com',
+        'translate.googleapis.com',
+    ],
+    'connect-src': [
+        "'self'",
+        '*.nav.no',
+        'oidc-ver2.difi.no/idporten-oidc-provider/authorize',
+        'idporten-ver2.difi.no/opensso/SSORedirect/metaAlias/norge.no/idp4',
+        'amplitude.nav.no',
+        'data.brreg.no',
+        '*.psplugin.com',
+        '*.hotjar.com',
+        '*.vc.hotjar.com',
+        '*.hotjar.io',
+        '*.vc.hotjar.io',
+        'wss://*.hotjar.com',
+        'api.puzzel.com',
+        'nav.boost.ai',
+        'sentry.gc.nav.no',
+        'ta-survey-v2.herokuapp.com',
+    ],
+    'font-src': ['data:', '*.psplugin.com', '*.hotjar.com', 'fonts.gstatic.com', 'www.nav.no'],
+    'frame-src': ['vars.hotjar.com', '*.nav.no'],
+    'img-src': [
+        "'self'",
+        '*.hotjar.com',
+        'www.gstatic.com',
+        'translate.google.com',
+        '*.nav.no',
+        'data:',
+        'www.googletagmanager.com',
+    ],
+    'report-uri': [
+        'https://sentry.gc.nav.no/api/13/security/?sentry_key=c4ef091d1fb54f01a7f808e621b28948',
+    ],
+    'worker-src': ["'self'", 'blob:'],
 };
 
 function getCspValue() {
-  return Object.entries(contentSecurityPolicy)
-  .map((entry) => `${entry[0]} ${entry[1].join(' ')}`)
-  .join('; ');
+    return Object.entries(contentSecurityPolicy)
+        .map((entry) => `${entry[0]} ${entry[1].join(' ')}`)
+        .join('; ');
 }
 
 module.exports = getCspValue;
