fd72a01aed44f1bc5704f87e23704e36f5600ec8
runarskagestad <runar.skagestad@nav.no>
2023-01-02 10:57:06 +0100

    change apmlitude project

--- src/amplitude/client.ts
+++ src/amplitude/client.ts
@@ -1,23 +1,14 @@
 import amplitude from 'amplitude-js';
 
-const AmplitudeBucket = {
-  ARBEIDSGIVER_PROD: 'a8243d37808422b4c768d31c88a22ef4',
-  ARBEIDSGIVER_DEV: '6ed1f00aabc6ced4fd6fcb7fcdc01b30',
-};
-
 function initializeAmplitudeClient(): AnalyticsClient {
-  const bucketId =
-      window.location.hostname === 'arbeidsgiver.nav.no'
-          ? AmplitudeBucket.ARBEIDSGIVER_PROD
-          : AmplitudeBucket.ARBEIDSGIVER_DEV;
 
   const amplitudeInstance = amplitude.getInstance();
-  amplitudeInstance.init(bucketId, '', {
-    apiEndpoint: 'amplitude.nav.no/collect',
+  amplitudeInstance.init('default', '', {
+    apiEndpoint: 'amplitude.nav.no/collect-auto',
     saveEvents: false,
     includeUtm: true,
-    batchEvents: false,
     includeReferrer: true,
+    platform: window.location.toString(),
   });
 
   return amplitudeInstance;
