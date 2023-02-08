5e217a85909a3439b7bd28bfbf1e86e687cd3073
runarskagestad <runar.skagestad@nav.no>
2023-02-08 15:20:08 +0100

    legger tilbake side for manglende rettighet til valgt bedrift

---
+++ src/FeilSider/ManglerRettigheterIAltinnSide/ManglerRettigheterIAltinnSide.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import informasjonsirkelSvg from './informasjon-sirkel.svg';
+import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import './ManglerRettigheterIAltinnSide.less';
+import Lenke from 'nav-frontend-lenker';
+import { RestAltinnOrganisasjoner } from '../../api/altinnorganisasjon-api';
+import { OrganisasjonerMedTilgangListe } from './OrganisasjonerMedTilgangListe/OrganisasjonerMedTilgangListe';
+import { BeOmTilgang } from './BeOmTilgang/BeOmTilgang';
+
+interface Props {
+    restOrganisasjonerMedStatistikk: RestAltinnOrganisasjoner;
+}
+
+export const ManglerRettigheterIAltinnSide: React.FunctionComponent<Props> = ({
+    restOrganisasjonerMedStatistikk,
+}) => {
+    return (
+        <div className="mangler-rettigheter-i-altinn__wrapper">
+            <div className="mangler-rettigheter-i-altinn">
+                <div className="mangler-rettigheter-i-altinn__tekst_og_ikon">
+                    <img
+                        src={informasjonsirkelSvg}
+                        className="mangler-rettigheter-i-altinn__tekst_og_ikon__ikon"
+                        alt=""
+                    />
+                    <Systemtittel tag="h2">Du mangler rettigheter i Altinn</Systemtittel>
+                </div>
+
+                <Normaltekst className="mangler-rettigheter-i-altinn__overskrift">
+                    Du har ikke Altinn-tilgangen du trenger for å se Sykefraværsstatistikk for denne
+                    virksomheten. Bytt til en virksomhet der du har tilgang eller be om tilgang i
+                    Altinn for denne virksomheten.
+                </Normaltekst>
+                <BeOmTilgang />
+
+                <OrganisasjonerMedTilgangListe
+                    restOrganisasjonerMedStatistikk={restOrganisasjonerMedStatistikk}
+                />
+                <Element className="mangler-rettigheter-i-altinn__mer-info">
+                    Lenker til mer informasjon
+                </Element>
+                <Lenke
+                    className="mangler-rettigheter-i-altinn__lenke"
+                    href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver/informasjon-om-tilgangsstyring"
+                >
+                    Les mer om hvordan tilgangsstyringen i Altinn fungerer
+                </Lenke>
+            </div>
+        </div>
+    );
+};
