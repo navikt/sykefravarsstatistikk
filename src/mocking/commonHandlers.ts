a40797b8a579d6edc3c31fc2ade9f5a1aa6d1315
runarskagestad <runar.skagestad@nav.no>
2023-02-13 12:57:22 +0100

    fix merge conflicts

--- src/mocking/commonHandlers.ts
+++ src/mocking/commonHandlers.ts
@@ -1,25 +1,28 @@
 import { rest } from 'msw';
 import { lagMockHistorikkForNæring } from './sykefraværshistorikk-mock';
 import { aggregertMockData } from './aggregert-mock';
-import { getOrganisasjonerMedStatistikktilgangMock, getOrganisasjonerMock } from './altinn-mock';
+import {
+    getOrganisasjonerBrukerHarIaRettigheterTilMock,
+    getOrganisasjonerMock,
+} from './altinn-mock';
 import { getMockPubliseringsdatoer } from './mock-publiseringsdatoer';
-import { underenhetMock } from './enhetsregisteret-mock';
+import { underenheterResponseMock } from '../enhetsregisteret/api/mocks/underenheter-api-mocks';
 
 export const commonHandlers = [
     // La alle handlere her være uavhengig av orgnummer
     // TODO: Sett opp egne handlers for spesifikke orgnumre
     rest.get(
-      '/sykefravarsstatistikk/api/:orgnr/sykefravarshistorikk/kvartalsvis',
-      (_, res, ctx) => {
-          return res(ctx.status(200), ctx.json(lagMockHistorikkForNæring()));
-      }
+        '/sykefravarsstatistikk/api/:orgnr/sykefravarshistorikk/kvartalsvis',
+        (_, res, ctx) => {
+            return res(ctx.status(200), ctx.json(lagMockHistorikkForNæring()));
+        }
     ),
 
     rest.get(
-      '/sykefravarsstatistikk/api/:orgnr/v1/sykefravarshistorikk/aggregert',
-      (_, res, ctx) => {
-          return res(ctx.status(200), ctx.json(aggregertMockData));
-      }
+        '/sykefravarsstatistikk/api/:orgnr/v1/sykefravarshistorikk/aggregert',
+        (_, res, ctx) => {
+            return res(ctx.status(200), ctx.json(aggregertMockData));
+        }
     ),
 
     rest.get('/sykefravarsstatistikk/api/organisasjoner', (_, res, ctx) => {
@@ -31,30 +34,29 @@ export const commonHandlers = [
     }),
 
     rest.get('/sykefravarsstatistikk/api/organisasjoner/statistikk', (_, res, ctx) => {
-        return res(ctx.status(200), ctx.json(getOrganisasjonerMedStatistikktilgangMock()));
+        return res(ctx.status(200), ctx.json(getOrganisasjonerBrukerHarIaRettigheterTilMock()));
     }),
 
     rest.get('https://data.brreg.no/enhetsregisteret/api/enheter/', (req, res, ctx) => {
         // TODO: Sjekk om denne brukes
         const orgnr = req.url.toString().match(/[0-9]{9}/)![0];
         return res(
-          ctx.status(200),
-          ctx.json({
-              orgnr: orgnr,
-              institusjonellSektorkode: { kode: '6500', beskrivelse: 'Offentlig sektor' },
-          })
+            ctx.status(200),
+            ctx.json({
+                orgnr: orgnr,
+                institusjonellSektorkode: { kode: '6500', beskrivelse: 'Offentlig sektor' },
+            })
         );
     }),
 
     rest.get('https://data.brreg.no/enhetsregisteret/api/underenheter/', (req, res, ctx) => {
-        // TODO: Sjekk om denne brukes
         const orgnr = req.url.toString().match(/[0-9]{9}/)![0];
         return res(
-          ctx.status(200),
-          ctx.json({
-              ...underenhetMock,
-              organisasjonsnummer: orgnr,
-          })
+            ctx.status(200),
+            ctx.json({
+                ...underenheterResponseMock,
+                organisasjonsnummer: orgnr,
+            })
         );
     }),
 
