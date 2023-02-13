4e5d3ee475c74c3af8fbc7fef57ffae1729b2920
runarskagestad <runar.skagestad@nav.no>
2023-02-13 15:40:23 +0100

    remove AB-test

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -1,9 +1,5 @@
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom/extend-expect';
-import { configure } from 'enzyme';
 import { mswServer } from './mswServer';
-
-configure({ adapter: new Adapter() });
 
 beforeAll(() => mswServer.listen());
 afterEach(() => mswServer.resetHandlers());
