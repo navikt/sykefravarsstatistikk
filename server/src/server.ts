b90594f1b62cba6a97260e0aa2dd76867bd7dee3
Runar Skagestad <runska@gmail.com>
2023-05-11 10:16:50 +0200

    Starter ikke opp app hvis vi kjører på ekstern dev

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,22 @@
 import path from 'path';
 import express from 'express';
 import mustacheExpress from 'mustache-express';
-import cookieParser from 'cookie-parser';
 import Prometheus from 'prom-client';
 import { fileURLToPath } from 'node:url';
-import { sykefraværsstatistikkApiProxy } from './proxy.js';
-import { iaTjenesterMetrikkerProxy } from './iaTjenesterMetrikkerProxy.js';
 import { applyNotifikasjonMiddleware } from './brukerapi-proxy-middleware.js';
 import { contentHeaders } from './contentHeaders.js';
 import { loggingHandler, logger } from './backend-logger.js';
 import { requestLoggingMiddleware } from './requestLogging.js';
 import { getKalkulatorRedirectUrl, getTemplateValues } from './environment.js';
-import { applyWonderwallLoginRedirect } from './authentication/wonderwall.js';
-import { initIdporten } from './authentication/idporten.js';
-import { initTokenXClient } from './authentication/tokenx.js';
+
 import { BASE_PATH } from './common.js';
+import { applyWonderwallLoginRedirect } from "./authentication/wonderwall.js";
 
 const buildPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../build');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
-
-Prometheus.collectDefaultMetrics();
+const { PORT = 3000, NAIS_APP_NAME = 'local' } = process.env;
 
 const renderAppMedTemplateValues = (templateValues) => {
     return new Promise((resolve, reject) => {
@@ -41,14 +35,8 @@ const renderAppMedTemplateValues = (templateValues) => {
 
 const startServer = async (html) => {
     logger.info('Starting server: server.ts');
-    app.use(cookieParser());
 
-    await initIdporten();
-    await initTokenXClient();
-
-    app.use(sykefraværsstatistikkApiProxy);
-    app.use(iaTjenesterMetrikkerProxy);
-
+    Prometheus.collectDefaultMetrics();
     applyWonderwallLoginRedirect(app);
     applyNotifikasjonMiddleware(app);
 
@@ -93,12 +81,18 @@ const startServer = async (html) => {
     });
 };
 
-getTemplateValues()
-    .then(renderAppMedTemplateValues, (error) => {
-        logger.error({ error }, 'Kunne ikke hente Template verdier');
-        process.exit(1);
-    })
-    .then(startServer, (error) => {
-        logger.error({ error }, 'Kunne ikke rendre app ');
-        process.exit(1);
-    });
+async function main() {
+    const templateValues = await getTemplateValues();
+    const html = await renderAppMedTemplateValues(templateValues);
+
+    if (NAIS_APP_NAME === 'sykefravarsstatistikk-mock') {
+        logger.info('App running on "ekstern dev". Will not start the server.');
+    } else {
+        await startServer(html);
+    }
+}
+
+main().catch((error) => {
+    logger.error({ error }, 'Kunne ikke hente eller rendre app');
+    process.exit(1);
+});
