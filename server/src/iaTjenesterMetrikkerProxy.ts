d11cef2853b27855e1a599d0e0345ca9e9d45477
Runar Skagestad <runar.skagestad@nav.no>
2023-05-12 12:09:16 +0200

    TokenX-forenkling (#452)
    * Forenkler tokenx-implementasjonen ved å bruke idportenTokenExchangeMiddleware

--- server/src/iaTjenesterMetrikkerProxy.ts
+++ server/src/iaTjenesterMetrikkerProxy.ts
@@ -1,35 +1,33 @@
 import { createProxyMiddleware, Options } from 'http-proxy-middleware';
 import { logger } from './backend-logger.js';
-import { exchangeToken } from "./authentication/tokenx.js";
+import { Express } from 'express';
+import { idportenTokenExchangeMiddleware } from "@navikt/tokenx-middleware";
 
-function getProxyConfig(): Options {
-    const {
-        IA_TJENESTER_METRIKKER_BASE_URL = 'http://localhost:9090/ia-tjenester-metrikker',
-        IA_TJENESTER_METRIKKER_AUDIENCE,
-    } = process.env;
-
-    return {
-        target: IA_TJENESTER_METRIKKER_BASE_URL,
-        changeOrigin: true,
-        pathRewrite: { '/sykefravarsstatistikk/proxy/ia-tjenester-metrikker': '' },
-        router: async (req) => {
-            const tokenSet = await exchangeToken(req, IA_TJENESTER_METRIKKER_AUDIENCE);
-            if (!tokenSet?.expired() && tokenSet?.access_token) {
-                req.headers['authorization'] = `Bearer ${tokenSet.access_token}`;
-            }
-            return undefined;
-        },
-        secure: true,
-        xfwd: true,
-        logLevel: 'info',
-        logProvider: () => logger,
-        onError: (err) => {
-            logger.error('Error in ia-tjenester-metrikker proxy: ' + err);
-        },
-    };
+export function applyIaTjenesterMetrikkerProxyMiddlewares(server: Express) {
+    const iaTjenesterMetrikkerProxy = createProxyMiddleware(proxyConfig);
+    server.use(
+        '/sykefravarsstatistikk/proxy/ia-tjenester-metrikker',
+        idportenTokenExchangeMiddleware(IA_TJENESTER_METRIKKER_AUDIENCE),
+        iaTjenesterMetrikkerProxy
+    );
 }
 
-export const iaTjenesterMetrikkerProxy = createProxyMiddleware(
-    '/sykefravarsstatistikk/proxy/ia-tjenester-metrikker',
-    getProxyConfig()
-);
+const {
+    IA_TJENESTER_METRIKKER_BASE_URL = 'http://localhost:9090/ia-tjenester-metrikker',
+    IA_TJENESTER_METRIKKER_AUDIENCE,
+} = process.env;
+
+const proxyConfig: Options = {
+    target: IA_TJENESTER_METRIKKER_BASE_URL,
+    changeOrigin: true,
+    pathRewrite: { '/sykefravarsstatistikk/proxy/ia-tjenester-metrikker': '' },
+    secure: true,
+    xfwd: true,
+    logLevel: 'info',
+    logProvider: () => logger,
+    onError: (err) => {
+        logger.error('Error in ia-tjenester-metrikker proxy: ' + err);
+    },
+};
+
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,25 @@
 import path from 'path';
 import express from 'express';
 import mustacheExpress from 'mustache-express';
-import cookieParser from 'cookie-parser';
 import Prometheus from 'prom-client';
 import { fileURLToPath } from 'node:url';
-import { sykefraværsstatistikkApiProxy } from './proxy.js';
-import { iaTjenesterMetrikkerProxy } from './iaTjenesterMetrikkerProxy.js';
-import { applyNotifikasjonMiddleware } from './brukerapi-proxy-middleware.js';
+import { applySykefraværsstatistikkApiProxyMiddlewares } from './proxy.js';
+import { applyIaTjenesterMetrikkerProxyMiddlewares } from './iaTjenesterMetrikkerProxy.js';
+import { applyNotifikasjonProxyMiddlewares } from './brukerapi-proxy-middleware.js';
 import { contentHeaders } from './contentHeaders.js';
 import { loggingHandler, logger } from './backend-logger.js';
 import { requestLoggingMiddleware } from './requestLogging.js';
 import { getKalkulatorRedirectUrl, getTemplateValues } from './environment.js';
-import { applyWonderwallLoginRedirect } from './authentication/wonderwall.js';
-import { initIdporten } from './authentication/idporten.js';
-import { initTokenXClient } from './authentication/tokenx.js';
 import { BASE_PATH } from './common.js';
+import { applyWonderwallLoginRedirect } from './wonderwall.js';
+import { setupIsAlive, setupIsReady } from './healthcheck.js';
+import { setupMetricsEndpoint } from './prometheus.js';
 
 const buildPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../build');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
-
-Prometheus.collectDefaultMetrics();
+const { PORT = 3000, NAIS_APP_NAME = 'local' } = process.env;
 
 const renderAppMedTemplateValues = (templateValues) => {
     return new Promise((resolve, reject) => {
@@ -41,16 +38,14 @@ const renderAppMedTemplateValues = (templateValues) => {
 
 const startServer = async (html) => {
     logger.info('Starting server: server.ts');
-    app.use(cookieParser());
 
-    await initIdporten();
-    await initTokenXClient();
-
-    app.use(sykefraværsstatistikkApiProxy);
-    app.use(iaTjenesterMetrikkerProxy);
+    Prometheus.collectDefaultMetrics();
 
     applyWonderwallLoginRedirect(app);
-    applyNotifikasjonMiddleware(app);
+
+    applySykefraværsstatistikkApiProxyMiddlewares(app);
+    applyIaTjenesterMetrikkerProxyMiddlewares(app);
+    applyNotifikasjonProxyMiddlewares(app);
 
     app.disable('x-powered-by');
     app.use(contentHeaders);
@@ -72,13 +67,9 @@ const startServer = async (html) => {
         loggingHandler(req, res);
     });
 
-    app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
-    app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
-    app.get(`${BASE_PATH}/metrics`, async (req, res) => {
-        const metrics = await Prometheus.register.metrics();
-        res.set('Content-Type', Prometheus.register.contentType);
-        res.send(metrics);
-    });
+    await setupIsAlive(app);
+    await setupIsReady(app);
+    await setupMetricsEndpoint(app);
 
     app.get(BASE_PATH, (req, res) => {
         res.send(html);
@@ -93,12 +84,18 @@ const startServer = async (html) => {
     });
 };
 
-getTemplateValues()
-    .then(renderAppMedTemplateValues, (error) => {
-        logger.error({ error }, 'Kunne ikke hente Template verdier');
-        process.exit(1);
-    })
-    .then(startServer, (error) => {
-        logger.error({ error }, 'Kunne ikke rendre app ');
-        process.exit(1);
-    });
+async function main() {
+    const templateValues = await getTemplateValues();
+    const html = await renderAppMedTemplateValues(templateValues);
+
+    if (NAIS_APP_NAME === 'sykefravarsstatistikk-mock') {
+        logger.info('App running on "ekstern dev". Will not start the server.');
+    } else {
+        await startServer(html);
+    }
+}
+
+main().catch((error) => {
+    logger.error({ error }, 'Kunne ikke hente eller rendre app');
+    process.exit(1);
+});
