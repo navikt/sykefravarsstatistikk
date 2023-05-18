49ddf0657939b62b1d2f8ccd7edacbc787fc49f9
erlendaxelsson <erlend.axelsson@gmail.com>
2023-05-18 19:23:27 +0200

    Codemod migration Lenkepanel -> LinkPanel

--- src/FeilSider/ManglerRettigheterIAltinnSide/BeOmTilgang/BeOmTilgang.tsx
+++ src/FeilSider/ManglerRettigheterIAltinnSide/BeOmTilgang/BeOmTilgang.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import Lenkepanel from 'nav-frontend-lenkepanel/lib';
+import { LinkPanel } from '@navikt/ds-react';
 import { ReactComponent as AltinnLogo } from './altinn-logo.svg';
 import './BeOmTilgang.less';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
@@ -11,25 +11,28 @@ const beOmTilgangTilSykefraværsstatistikkIAltinnLink = (orgnr: string | undefin
 export const BeOmTilgang: FunctionComponent = () => {
     const orgnr = useOrgnr();
     return (
-        <Lenkepanel
-            border
-            tittelProps="normaltekst"
-            href={beOmTilgangTilSykefraværsstatistikkIAltinnLink(orgnr)}
-        >
-            <span className="be-om-tilgang">
-                <span className="be-om-tilgang__svg">
-                    <AltinnLogo />
+        <LinkPanel border href={beOmTilgangTilSykefraværsstatistikkIAltinnLink(orgnr)}>
+            <LinkPanel.Title>
+                <span className="be-om-tilgang">
+                    <span className="be-om-tilgang__svg">
+                        <AltinnLogo />
+                    </span>
+                    <span className="be-om-tilgang__tekst">
+                        <Undertittel tag="span" className="be-om-tilgang__tittel">
+                            Be om tilgang i Altinn
+                        </Undertittel>
+                        <Normaltekst tag="span">
+                            Gå til Altinn og be om tilgang til tjenesten. Du kan velge hvem i
+                            virksomheten som får forespørselen.
+                        </Normaltekst>
+                    </span>
                 </span>
-                <span className="be-om-tilgang__tekst">
-                    <Undertittel tag="span" className="be-om-tilgang__tittel">
-                        Be om tilgang i Altinn
-                    </Undertittel>
-                    <Normaltekst tag="span">
-                        Gå til Altinn og be om tilgang til tjenesten. Du kan velge hvem i
-                        virksomheten som får forespørselen.
-                    </Normaltekst>
-                </span>
-            </span>
-        </Lenkepanel>
+            </LinkPanel.Title>
+            {
+                // TODO: sjekk at LinkPanel.Title og LinkPanel.Description er satt riktig,
+                // transform kan ikke programatisk skille hva som skal være hva
+                // hvis du brukte tittelprops eller linkCreator så er du nødt til å fikse opp dette selv da programatisk migrering der er utenfor scope.
+            }
+        </LinkPanel>
     );
 };
