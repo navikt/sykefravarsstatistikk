089068c96e39aba6b74849bbb12e36f30323c19c
Runar Skagestad <runska@gmail.com>
2023-05-31 16:42:52 +0200

    fiks tester og reverser endringer på EksternLenke.tsx

--- src/felleskomponenter/EksternLenke/EksternLenke.tsx
+++ src/felleskomponenter/EksternLenke/EksternLenke.tsx
@@ -1,14 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import { Link, LinkProps } from '@navikt/ds-react';
-import { ExternalLinkIcon } from '@navikt/aksel-icons';
+import {
+    ExternalLinkIcon
+} from '@navikt/aksel-icons';
 import classNames from 'classnames';
 import { sendNavigereEvent } from '../../amplitude/events';
 import { WithRequired } from '../../utils/app-utils';
 
 const EksternLenke: FunctionComponent<WithRequired<LinkProps, 'href'>> = ({
-    href,
-    className,
     children: lenketekst,
+    className,
+    ...lenkeProperties
 }) => {
     if (typeof lenketekst !== 'string') {
         throw Error('EksternLenke støttes bare av tekstlenker.');
@@ -16,15 +18,16 @@ const EksternLenke: FunctionComponent<WithRequired<LinkProps, 'href'>> = ({
 
     return (
         <Link
+            {...lenkeProperties}
             className={classNames(className)}
             target="_blank"
             rel="noopener noreferrer"
             onClick={() => {
-                sendNavigereEvent(href, lenketekst);
+                sendNavigereEvent(lenkeProperties.href, lenketekst);
             }}
         >
             {lenketekst}
-            <ExternalLinkIcon title="Ekstern lenke" />
+            <ExternalLinkIcon  />
         </Link>
     );
 };
